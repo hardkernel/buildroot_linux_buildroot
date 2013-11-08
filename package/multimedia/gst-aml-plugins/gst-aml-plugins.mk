bab3ac62f0f577c97b5fa3c4710885de6d8b6544
yvonne.chen <yvonne.chen@amlogic.com>
2013-11-08 09:54:53 +0800

    change mk file to copy source code automatically from local dir

--- package/multimedia/gst-aml-plugins/gst-aml-plugins.mk
+++ package/multimedia/gst-aml-plugins/gst-aml-plugins.mk
@@ -4,7 +4,9 @@
 #
 #############################################################
 GST_AML_PLUGINS_VERSION = 0.11.0
-GST_AML_PLUGINS_SITE =file://$(TOPDIR)/package/multimedia/gst-aml-plugins/
+#GST_AML_PLUGINS_SITE =file://$(TOPDIR)/package/multimedia/gst-aml-plugins/
+GST_AML_PLUGINS_SITE = $(TOPDIR)/package/multimedia/gst-aml-plugins/gst-aml-plugins-0.11.0
+GST_AML_PLUGINS_SITE_METHOD = local
 
 GST_FSL_PLUGINS_INSTALL_STAGING = YES
 GST_FSL_PLUGINS_AUTORECONF = YES
