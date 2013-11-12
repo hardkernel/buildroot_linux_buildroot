0ef11ea5c9e84d41530c2d71bedb794bcd99b3d0
Robin Zhu <robin.zhu@amlogic.com>
2013-11-11 18:18:58 -0800

    downgrade icu from 5.1.2 to 4.2.1 because our toolchain fail to build 5.1.2.

--- package/icu/icu.mk
+++ package/icu/icu.mk
@@ -1,30 +1,26 @@
-################################################################################
+#############################################################
 #
-# icu
+# International Components for Unicode
 #
-################################################################################
+#############################################################
 
-ICU_VERSION = 51.2
+ICU_VERSION = 4.2.1
 ICU_SOURCE = icu4c-$(subst .,_,$(ICU_VERSION))-src.tgz
 ICU_SITE = http://download.icu-project.org/files/icu4c/$(ICU_VERSION)
-ICU_LICENSE = ICU License
-ICU_LICENSE_FILES = license.html
-
 ICU_DEPENDENCIES = host-icu
 ICU_INSTALL_STAGING = YES
 ICU_CONFIG_SCRIPTS = icu-config
-ICU_CONF_OPT = --with-cross-build=$(HOST_ICU_DIR)/source --disable-samples \
-		--disable-tests
-HOST_ICU_CONF_OPT = \
-	--disable-samples \
-	--disable-tests \
-	--disable-extras \
-	--disable-icuio \
-	--disable-layout \
-	--disable-renaming
-ICU_MAKE = $(MAKE1)
+ICU_CONF_OPT = --with-cross-build=$(HOST_ICU_DIR)/source --enable-samples=no --enable-tests=no --enable-extras=no --enable-renaming=no --enable-static=no
+HOST_ICU_CONF_OPT = --enable-samples=no --enable-tests=no --disable-icuio --disable-layout --enable-renaming=no --enable-static=no
 ICU_SUBDIR = source
 HOST_ICU_SUBDIR = source
+ICU_MAKE = $(MAKE1)
+
+define ICU_PREFIX_FIXUP
+	$(SED) "s,^default_prefix=.*,default_prefix=\'$(STAGING_DIR)/usr\',g" $(STAGING_DIR)/usr/bin/icu-config
+endef
+
+ICU_POST_INSTALL_TARGET_HOOKS += ICU_PREFIX_FIXUP
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
