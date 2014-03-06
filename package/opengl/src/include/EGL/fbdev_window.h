c996bb3081d0e2f0244b061ae321460211846998
Robin Zhu <robin.zhu@amlogic.com>
2014-03-06 12:52:47 -0800

    update opengl header files

--- package/opengl/src/include/EGL/fbdev_window.h
+++ package/opengl/src/include/EGL/fbdev_window.h
@@ -1,7 +1,7 @@
 /*
  * This confidential and proprietary software may be used only as
  * authorised by a licensing agreement from ARM Limited
- * (C) COPYRIGHT 2008-2012 ARM Limited
+ * (C) COPYRIGHT 2008-2013 ARM Limited
  * ALL RIGHTS RESERVED
  * The entire notice above must be reproduced on all authorised
  * copies and copies may only be made to the extent permitted
@@ -23,11 +23,11 @@ extern "C" {
 typedef enum
 {
 	FBDEV_PIXMAP_DEFAULT = 0,
-	FBDEV_PIXMAP_SUPPORTS_UMP = (1<<0),
-	FBDEV_PIXMAP_ALPHA_FORMAT_PRE = (1<<1),
-	FBDEV_PIXMAP_COLORSPACE_sRGB = (1<<2),
-	FBDEV_PIXMAP_EGL_MEMORY = (1<<3),       /* EGL allocates/frees this memory */
-	FBDEV_PIXMAP_DMA_BUF = (1<<4),
+	FBDEV_PIXMAP_SUPPORTS_UMP = (1 << 0),
+	FBDEV_PIXMAP_ALPHA_FORMAT_PRE = (1 << 1),
+	FBDEV_PIXMAP_COLORSPACE_sRGB = (1 << 2),
+	FBDEV_PIXMAP_EGL_MEMORY = (1 << 3),     /* EGL allocates/frees this memory */
+	FBDEV_PIXMAP_DMA_BUF = (1 << 4),
 } fbdev_pixmap_flags;
 
 typedef struct fbdev_window
@@ -53,10 +53,11 @@ typedef struct fbdev_pixmap
 } fbdev_pixmap;
 
 #if MALI_USE_DMA_BUF
-struct fbdev_dma_buf {
+struct fbdev_dma_buf
+{
 	int fd;
 	int size;
-	void* ptr;
+	void *ptr;
 };
 #endif
 
