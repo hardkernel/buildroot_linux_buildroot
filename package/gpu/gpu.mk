379a8be0fac8c698245917f0a2430316d3928a2f
Robin Zhu <robin.zhu@amlogic.com>
2014-03-07 15:22:26 -0800

    fix gpu BR2_PACKAGE_GPU_CUSTOM_TARBALL_LOCATION name from wrong BR2_PACKAGE_GPU_CUSTOM_TARBALL to match Config.in

--- package/gpu/gpu.mk
+++ package/gpu/gpu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 GPU_VERSION = $(call qstrip,$(BR2_PACKAGE_GPU_VERSION))
-ifneq ($(BR2_PACKAGE_GPU_CUSTOM_TARBALL),"")
+ifneq ($(BR2_PACKAGE_GPU_CUSTOM_TARBALL_LOCATION),"")
 GPU_TARBALL = $(call qstrip,$(BR2_PACKAGE_GPU_CUSTOM_TARBALL_LOCATION))
 GPU_SITE    = $(patsubst %/,%,$(dir $(GPU_TARBALL)))
 GPU_SOURCE  = $(notdir $(GPU_TARBALL))
