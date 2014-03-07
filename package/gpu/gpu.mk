b6372ce05384df0e3bbd8e7a899c7cb307713be2
Matthew.Shyu <matthew.shyu@amlogic.com>
2014-03-07 16:19:05 +0800

    fix gpu release download

--- package/gpu/gpu.mk
+++ package/gpu/gpu.mk
@@ -3,7 +3,12 @@
 # amlogic gpu driver
 #
 ################################################################################
-
 GPU_VERSION = $(call qstrip,$(BR2_PACKAGE_GPU_VERSION))
+ifneq ($(BR2_PACKAGE_GPU_CUSTOM_TARBALL),"")
+GPU_TARBALL = $(call qstrip,$(BR2_PACKAGE_GPU_CUSTOM_TARBALL_LOCATION))
+GPU_SITE    = $(patsubst %/,%,$(dir $(GPU_TARBALL)))
+GPU_SOURCE  = $(notdir $(GPU_TARBALL))
+else
 GPU_SITE = $(call qstrip,$(BR2_PACKAGE_GPU_GIT_URL))
+endif
 $(eval $(generic-package))
