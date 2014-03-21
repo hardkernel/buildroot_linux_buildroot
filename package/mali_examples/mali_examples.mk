6c79d183a4b9cbd9e3ef86ba512359bd9132b540
Matthew.Shyu <matthew.shyu@amlogic.com>
2014-03-21 19:06:40 +0800

    rename to mali examples

--- package/mali_examples/mali_examples.mk
+++ package/mali_examples/mali_examples.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-MALI_UTILITY_VERSION = 2.0.0.9444
-MALI_UTILITY_SITE = $(TOPDIR)/package/mali_utility/bin
-MALI_UTILITY_SITE_METHOD = local
+MALI_EXAMPLES_VERSION = 2.0.0.9444
+MALI_EXAMPLES_SITE = $(TOPDIR)/package/mali_examples/bin
+MALI_EXAMPLES_SITE_METHOD = local
 
-define MALI_UTILITY_INSTALL_TARGET_CMDS
+define MALI_EXAMPLES_INSTALL_TARGET_CMDS
 	cp -rf $(@D)/arm/ $(TARGET_DIR)/usr/share/
 endef
 
