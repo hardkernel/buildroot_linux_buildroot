5bc120f16bcb84d49bf950e694e1db0165323c10
Matthew.Shyu <matthew.shyu@amlogic.com>
2014-04-17 18:46:30 +0800

    Add libraries

--- package/opengl/opengl.mk
+++ package/opengl/opengl.mk
@@ -27,13 +27,13 @@ endif
 $(OPENGL_DIR)/.unpacked:
 	mkdir -p $(OPENGL_DIR)
 	cp -arf ./package/opengl/src/* $(OPENGL_DIR)
-	cp $(OPENGL_DIR)/lib/$(MALI_LIB_DIR)/libMali.so $(OPENGL_DIR)/lib/libMali.so
+	cp --remove-destination $(OPENGL_DIR)/lib/$(MALI_LIB_DIR)/*.so* $(OPENGL_DIR)/lib/
 	touch $(OPENGL_DIR)/.unpacked
 
 $(OPENGL_DIR)/.installed: $(OPENGL_DIR)/.unpacked
 	cp -arf $(OPENGL_DIR)/* $(STAGING_DIR)/usr
 	cp -df $(OPENGL_DIR)/lib/*.so* $(TARGET_DIR)/usr/lib
-	install -m 755 $(OPENGL_DIR)/lib/$(MALI_LIB_DIR)/libMali.so $(TARGET_DIR)/usr/lib
+	install -m 755 $(OPENGL_DIR)/lib/$(MALI_LIB_DIR)/*.so* $(TARGET_DIR)/usr/lib
 	mkdir -p $(TARGET_DIR)/usr/lib/pkgconfig
 	install -m 644 $(OPENGL_DIR)/lib/pkgconfig/*.pc $(TARGET_DIR)/usr/lib/pkgconfig
 	touch $(OPENGL_DIR)/.installed
