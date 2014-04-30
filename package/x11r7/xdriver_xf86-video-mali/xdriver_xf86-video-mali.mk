bddb499e2295ed6aa14480606b351e8c2407a5d9
Matthew.Shyu <matthew.shyu@amlogic.com>
2014-04-30 14:39:11 +0800

    Add mali driver

---
+++ package/x11r7/xdriver_xf86-video-mali/xdriver_xf86-video-mali.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# xdriver_xf86-video-fbturbo -- video driver for framebuffer device
+#
+################################################################################
+
+XDRIVER_XF86_VIDEO_MALI_VERSION = master
+XDRIVER_XF86_VIDEO_MALI_SITE = git://github.com/linux-sunxi/xf86-video-mali.git
+XDRIVER_XF86_VIDEO_MALI_DEPENDENCIES = xserver_xorg-server xproto_xf86driproto xproto_fontsproto xproto_randrproto xproto_renderproto xproto_videoproto xproto_xproto
+XDRIVER_XF86_VIDEO_MALI_AUTORECONF = YES
+
+define XDRIVER_XF86_VIDEO_MALI_CHANGE_MODE
+	chmod +x $(@D)/configure
+endef
+XDRIVER_XF86_VIDEO_MALI_POST_PATCH_HOOKS += XDRIVER_XF86_VIDEO_MALI_CHANGE_MODE
+
+$(eval $(autotools-package))
