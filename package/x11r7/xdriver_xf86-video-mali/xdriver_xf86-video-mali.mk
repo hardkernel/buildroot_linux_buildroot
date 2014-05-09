a3a199dcbcce0ececba123b3af11bce26156a838
Matthew.Shyu <matthew.shyu@amlogic.com>
2014-05-09 10:55:41 +0800

    Use mali drv in X

--- package/x11r7/xdriver_xf86-video-mali/xdriver_xf86-video-mali.mk
+++ package/x11r7/xdriver_xf86-video-mali/xdriver_xf86-video-mali.mk
@@ -1,17 +1,19 @@
 ################################################################################
 #
-# xdriver_xf86-video-fbturbo -- video driver for framebuffer device
+# xdriver_xf86-video-mali -- video driver for mali 
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_MALI_VERSION = r3p0-04rel0
-XDRIVER_XF86_VIDEO_MALI_SITE = git://github.com/linux-sunxi/xf86-video-mali.git
+XDRIVER_XF86_VIDEO_MALI_VERSION = amlogic
+XDRIVER_XF86_VIDEO_MALI_SITE = $(TOPDIR)/package/x11r7/xdriver_xf86-video-mali/src
+XDRIVER_XF86_VIDEO_MALI_SITE_METHOD = local
 XDRIVER_XF86_VIDEO_MALI_DEPENDENCIES = xserver_xorg-server xproto_xf86driproto xproto_fontsproto xproto_randrproto xproto_renderproto xproto_videoproto xproto_xproto
 XDRIVER_XF86_VIDEO_MALI_AUTORECONF = YES
+XDRIVER_XF86_VIDEO_MALI_CONF_OPT += CPPFLAGS="-I$(@D)/ddk-include/include"
 
 define XDRIVER_XF86_VIDEO_MALI_CHANGE_MODE
 	chmod +x $(@D)/configure
 endef
-XDRIVER_XF86_VIDEO_MALI_POST_PATCH_HOOKS += XDRIVER_XF86_VIDEO_MALI_CHANGE_MODE
+XDRIVER_XF86_VIDEO_MALI_POST_RSYNC_HOOKS += XDRIVER_XF86_VIDEO_MALI_CHANGE_MODE
 
 $(eval $(autotools-package))
