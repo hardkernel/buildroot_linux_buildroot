6dd99267fc3c448c9b242227e26631926a79c428
Matthew.Shyu <matthew.shyu@amlogic.com>
2014-05-19 17:38:48 +0800

    port autotest client

--- package/autotest/autotest.mk
+++ package/autotest/autotest.mk
@@ -7,11 +7,11 @@ AUTOTEST_VERSION = master
 AUTOTEST_SITE = $(call github,autotest,autotest,$(AUTOTEST_VERSION))
 AUTOTEST_LICENSE = LGPLv3+
 AUTOTEST_LICENSE_FILES = LGPL_LICENSE
-AUTOTEST_DEPENDENCIES = host-python host-python-sphinx
+AUTOTEST_DEPENDENCIES = host-python host-python-sphinx autotest-client-tests
 AUTOTEST_SETUP_TYPE = distutils
 
 define AUTOTEST_ENV
-	export AUTOTEST_TOP_PATH=$(TARGET_DIR)/etc/autotest;
+	export AUTOTEST_TOP_PATH=$(TARGET_DIR)/etc/autotest; 
 endef
 
 $(eval $(python-package))
