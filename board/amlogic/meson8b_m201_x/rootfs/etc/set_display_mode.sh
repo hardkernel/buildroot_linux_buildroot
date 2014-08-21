c0a0b68dc6d9b910e4ab7aa3ff4fdbdcc11fc4e0
Matthew.Shyu <matthew.shyu@amlogic.com>
2014-08-21 13:50:17 +0800

    Change display to native mode

--- board/amlogic/meson8b_m201_x/rootfs/etc/set_display_mode.sh
+++ board/amlogic/meson8b_m201_x/rootfs/etc/set_display_mode.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
+HPD_STATE=/sys/class/amhdmitx/amhdmitx0/hpd_state
+DISP_CAP=/sys/class/amhdmitx/amhdmitx0/disp_cap
+DISP_MODE=/sys/class/display/mode
 
-old_state=1
-outputmode=$(cat /sys/class/display/mode)
-hpdstate=$(cat /sys/class/amhdmitx/amhdmitx0/hpd_state)
-old_state=$hpdstate
+hdmi=`cat $HPD_STATE`
+if [ $hdmi -eq 1 ]; then
+    mode=`awk -f /etc/display/get_hdmi_mode.awk $DISP_CAP`
+    echo $mode > $DISP_MODE
+fi
 
-outputmode=720p
+outputmode=$mode
+
+case $mode in
+    720*)
+        fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
+        ;;
+    1080*)
+        fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
+        ;;
+    *)
+        fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
+        outputmode=720p
+        ;;
+esac
+fbset -fb /dev/fb1 -g 32 32 32 32 32
+
+#old_state=1
+#outputmode=$(cat /sys/class/display/mode)
+#hpdstate=$(cat /sys/class/amhdmitx/amhdmitx0/hpd_state)
+#old_state=$hpdstate
+#
+#outputmode=720p
 #if [ "$hpdstate" = "1" ]; then
 #	if [ "$outputmode" = "480cvbs" -o "$outputmode" = "576cvbs" ] ; then
 #    outputmode=720p
