f23c9d4b42c00e2070ccb2e8b40b49e83bd82903
Matthew Shyu <matthew.shyu@amlogic.com>
2015-07-31 11:54:54 +0800

    Update more configurations per jiyu.yang

--- linux/linux.mk
+++ linux/linux.mk
@@ -309,6 +309,8 @@ define LINUX_KCONFIG_FIXUP_CMDS
 		$(call KCONFIG_ENABLE_OPT,CONFIG_DRM,$(@D)/.config)
 		$(call KCONFIG_ENABLE_OPT,CONFIG_DRM_MALI,$(@D)/.config)
 		$(call KCONFIG_ENABLE_OPT,CONFIG_FB_OSD2_CURSOR,$(@D)/.config)
+		$(call KCONFIG_ENABLE_OPT,CONFIG_UMP,$(@D)/.config)
+		$(call KCONFIG_ENABLE_OPT,CONFIG_MALI400_UMP,$(@D)/.config)
 		$(call KCONFIG_ENABLE_OPT,CONFIG_FB_AMLOGIC_UMP,$(@D)/.config)
                 cp -rf linux/mali_drm $(LINUX_DIR)/drivers/gpu/drm)
 	$(if $(BR2_TARGET_ROOTFS_CPIO),
