5e144f0ade8c1cdd0ca2828759772e29946cfb5f
Matthew Shyu <matthew.shyu@amlogic.com>
2015-08-14 15:34:06 +0800

    rm stressapptest source code

--- package/amlogic_test/amlogic_test.mk
+++ package/amlogic_test/amlogic_test.mk
@@ -1,22 +1,15 @@
 ################################################################################
 #
-# amlogic image packer
+# amlogic test
 #
 ################################################################################
 AMLOGIC_TEST_VERSION = 2015
 AMLOGIC_TEST_SITE = $(TOPDIR)/package/amlogic_test/
 AMLOGIC_TEST_SITE_METHOD = local
-
-define AMLOGIC_TEST_CONFIGURE_CMDS
-	(cd $(@D)/src/stressapptest; \
-	 ./configure \
-	 --host=$(BR2_TOOLCHAIN_EXTERNAL_PREFIX) \
-	)
-endef
+AMLOGIC_TEST_DEPENDENCIES = stressapptest
 
 define AMLOGIC_TEST_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/src/auto_suspend
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/src/stressapptest
 endef
 
 define AMLOGIC_TEST_INSTALL_TARGET_CMDS
@@ -26,7 +19,6 @@ define AMLOGIC_TEST_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/auto_reboot.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/disk-test.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/wifi_test.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/src/stressapptest/src/stressapptest $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
