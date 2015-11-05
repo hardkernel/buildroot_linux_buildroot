67401bddf23c50df7e2f535d54d82a8ed06a8231
Matthew Shyu <matthew.shyu@amlogic.com>
2015-11-05 18:12:52 +0800

    Add files for 905-X

--- package/gpu/gpu.mk
+++ package/gpu/gpu.mk
@@ -17,35 +17,44 @@ ifeq ($(BR2_PACKAGE_GPU_STANDALONE),y)
 GPU_DEPENDENCIES = linux
 endif
 
-ifeq ($(BR2_PACKAGE_GPU_STANDALONE),y)
-ifeq ($(BR2_PACKAGE_GPU_UMP),y)
-define GPU_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/mali ARCH=$(KERNEL_ARCH) \
-		CROSS_COMPILE=$(TARGET_CROSS) CONFIG_MALI400=m CONFIG_MALI450=m MALI400_UMP=y modules
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/ump ARCH=$(KERNEL_ARCH) \
-		CROSS_COMPILE=$(TARGET_CROSS) CONFIG_UMP=m modules
+ifeq ($(BR2_PACKAGE_GPU_MALI_DRM),y)
+MALI_DRM_BUILD_CMD = \
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/egl/x11/drm_module/mali_drm ARCH=$(KERNEL_ARCH) \
 		CROSS_COMPILE=$(TARGET_CROSS) modules
-endef
-define GPU_INSTALL_TARGET_CMDS
-	mkdir -p $(GPU_INSTALL_DIR)
-	$(INSTALL) -m 0666 $(@D)/mali/mali.ko $(GPU_INSTALL_DIR)
-	echo $(GPU_MODULE_DIR)/mali.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep
-	$(INSTALL) -m 0666 $(@D)/ump/ump.ko $(GPU_INSTALL_DIR)
-	echo $(GPU_MODULE_DIR)/ump.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep
-	$(INSTALL) -m 0666 $(@D)/egl/x11/drm_module/mali_drm/mali_drm.ko $(GPU_INSTALL_DIR)
+MALI_DRM_INSTALL_TARGET_CMDS = \
+	$(INSTALL) -m 0666 $(@D)/egl/x11/drm_module/mali_drm/mali_drm.ko $(GPU_INSTALL_DIR); \
 	echo $(GPU_MODULE_DIR)/mali_drm.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep
-endef
+endif
+ifeq ($(BR2_PACKAGE_GPU_UMP),y)
+MALI_BUILD_CMD = \
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/mali ARCH=$(KERNEL_ARCH) \
+		CROSS_COMPILE=$(TARGET_CROSS) CONFIG_MALI400=m CONFIG_MALI450=m MALI400_UMP=y modules;
+MALI_UMP_BUILD_CMD = \
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/ump ARCH=$(KERNEL_ARCH) \
+		CROSS_COMPILE=$(TARGET_CROSS) CONFIG_UMP=m modules
+MALI_UMP_INSTALL_TARGET_CMDS = \
+	$(INSTALL) -m 0666 $(@D)/ump/ump.ko $(GPU_INSTALL_DIR); \
+	echo $(GPU_MODULE_DIR)/ump.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep;
 else
-define GPU_BUILD_CMDS
+MALI_BUILD_CMD = \
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/mali ARCH=$(KERNEL_ARCH) \
 		CROSS_COMPILE=$(TARGET_CROSS) CONFIG_MALI400=m CONFIG_MALI450=m modules
-endef
 endif
+MALI_INSTALL_TARGETS_CMDS = \
+	$(INSTALL) -m 0666 $(@D)/mali/mali.ko $(GPU_INSTALL_DIR); \
+	echo $(GPU_MODULE_DIR)/mali.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep;
+
+ifeq ($(BR2_PACKAGE_GPU_STANDALONE),y)
+define GPU_BUILD_CMDS
+	$(MALI_BUILD_CMD)
+	$(MALI_UMP_BUILD_CMD) 
+	$(MALI_DRM_BUILD_CMD)
+endef
 define GPU_INSTALL_TARGET_CMDS
-	mkdir -p $(GPU_INSTALL_DIR)
-	$(INSTALL) -m 0666 $(@D)/mali/mali.ko $(GPU_INSTALL_DIR)
-	echo $(GPU_MODULE_DIR)/mali.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep
+	mkdir -p $(GPU_INSTALL_DIR);
+	$(MALI_INSTALL_TARGETS_CMDS)
+	$(MALI_UMP_INSTALL_TARGET_CMDS) 
+	$(MALI_DRM_INSTALL_TARGET_CMDS)
 endef
 endif
 $(eval $(generic-package))
