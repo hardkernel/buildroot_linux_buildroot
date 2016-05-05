63a58d3c94329e86ae37ec0512ca597472ad4b79
Yvonne Chen <yvonne.chen@amlogic.com>
2016-05-05 14:51:34 +0800

    merge aml_libs to libplayer to fix sw decoder compile error

--- package/multimedia/gst-aml-plugins1/gst-aml-plugins.mk
+++ package/multimedia/gst-aml-plugins1/gst-aml-plugins.mk
@@ -10,7 +10,7 @@ GST_AML_PLUGINS1_SITE_METHOD = local
 
 GST_AML_PLUGINS1_INSTALL_STAGING = YES
 GST_AML_PLUGINS1_AUTORECONF = YES
-GST_AML_PLUGINS1_DEPENDENCIES = gstreamer1 host-pkgconf aml_libs
+GST_AML_PLUGINS1_DEPENDENCIES = gstreamer1 host-pkgconf libplayer
 
 $(eval $(autotools-package))
 
