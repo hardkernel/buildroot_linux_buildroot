35454ce2d30da9f8734685b946b00dfe0ac89881
Yueguie He <yuegui.he@amlogic.com>
2017-05-15 21:57:43 +0800

    add ubifs for s400 && s420
      mesonaxg_s400
      mesonaxg_s400_32
      mesonaxg_s420
      mesonaxg_s420_32
    
    Change-Id: I23a28ca33479e6ad46f90df25d4e083bfcfa1f92

--- fs/ubifs/ubifs.mk
+++ fs/ubifs/ubifs.mk
@@ -21,7 +21,30 @@ UBIFS_OPTS += $(call qstrip,$(BR2_TARGET_ROOTFS_UBIFS_OPTS))
 ROOTFS_UBIFS_DEPENDENCIES = host-mtd
 
 define ROOTFS_UBIFS_CMD
-	$(HOST_DIR)/usr/sbin/mkfs.ubifs -d $(TARGET_DIR) $(UBIFS_OPTS) -o $@
+#	$(HOST_DIR)/usr/sbin/mkfs.ubifs -d $(TARGET_DIR) $(UBIFS_OPTS) -o $@
 endef
 
+DEVICE_DIR := $(patsubst "%",%,$(BR2_ROOTFS_OVERLAY))
+UPGRADE_DIR := $(DEVICE_DIR)../upgrade
+ifeq ($(BR2_TARGET_USBTOOL_AMLOGIC),y)
+ifeq ($(BR2_TARGET_UBOOT_AMLOGIC_2015),y)
+rootfs-usb-image-pack-ubifs:
+	cp -rf $(UPGRADE_DIR)/* $(BINARIES_DIR)
+	BINARIES_DIR=$(BINARIES_DIR) \
+	TOOL_DIR=$(HOST_DIR)/usr/bin \
+	$(HOST_DIR)/usr/bin/aml_upgrade_pkg_gen.sh \
+	$(BR2_TARGET_UBOOT_PLATFORM) $(BR2_TARGET_UBOOT_ENCRYPTION)
+else #BR2_TARGET_UBOOT_AMLOGIC_2015
+rootfs-usb-image-pack-ubifs:
+	cp -v -f $(UPGRADE_DIR)/platform.conf $(BINARIES_DIR)
+	$(HOST_DIR)/usr/bin/aml_image_v2_packer -r $(BINARIES_DIR)/usb_burn_package.conf $(BINARIES_DIR)/ $(BINARIES_DIR)/aml_upgrade_package.img
+endif #BR2_TARGET_UBOOT_AMLOGIC_2015
+ROOTFS_UBIFS_POST_TARGETS += rootfs-usb-image-pack-ubifs
+endif #BR2_TARGET_USBTOOL_AMLOGIC
+
+ifeq ($(BR2_TARGET_UBOOT_AMLOGIC_2015),y)
+SD_BOOT = $(BINARIES_DIR)/u-boot.bin.sd.bin
+else
+SD_BOOT = $(BINARIES_DIR)/u-boot.bin
+endif
 $(eval $(call ROOTFS_TARGET,ubifs))
