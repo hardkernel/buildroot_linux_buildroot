fb5c31b6650a2226739db6f6c6179fc04cb6eb6b
Lianghu Su <lianghu.su@amlogic.com>
2017-05-19 13:54:22 +0800

    PD#144626: add p231 and p241 build config.
    Change-Id: Iddb2469d6fb000e8ea9582c82afd6ec55edb40ae

--- build/setenv.sh
+++ build/setenv.sh
@@ -4,9 +4,7 @@ LOCAL_DIR=$(pwd)
 BUILDROOT_DIR=$LOCAL_DIR/buildroot
 BUILD_OUTPUT_DIR=$LOCAL_DIR/output
 
-DEFCONFIG_ARRAY=("mesonaxg_s400_32" "mesonaxg_s400" "mesonaxg_s420_32" "mesonaxg_s420" "mesongxl_p400_32_kernel49" "mesongxl_p400_kernel49" "mesongxl_p401_32_kernel49" "mesongxl_p401_kernel49" "mesongxl_p212_32_kernel49" "mesongxl_p212_kernel49" "mesongxm_q200_32_kernel49" "mesongxm_q200_kernel49" "meson8b_m200_kernel49" "mesongxl_p400" "mesongxl_p400_32" "mesongxl_p401" "mesongxl_p401_32" "mesongxb_p200" "mesongxb_p200_32" "mesongxb_p201" "mesongxb_p201_32" "mesongxl_p212" "mesongxl_p212_32" "mesongxl_p230" "mesongxl_p230_32" "mesongxm_q200" "mesongxm_q200_32" "meson8_k200" "meson8_k200b" "meson8b_m200" "meson8b_m201" "meson8m2_n200")
-BOARD_ARRAY=("s400" "s400" "s420" "s420" "p400" "p400" "p401" "p401" "p212" "p212" "q200" "q200" "m200" "p400" "p400" "p401" "p401" "p200" "p200" "p201" "p201" "p212" "p212" "p230" "p230" "q200" "q200" "k200" "k200b" "m200" "m201" "n200")
-BUILD_TYPE_ARRAY=("32" "64" "32" "64" "32" "64" "32" "64" "32" "64" "32" "64" "32" "64" "32" "64" "32" "64" "32" "64" "32" "64" "32" "64" "32" "64" "32" "32" "32" "32" "32" "32")
+DEFCONFIG_ARRAY=("mesonaxg_s400_32" "mesonaxg_s400" "mesonaxg_s420_32" "mesonaxg_s420" "mesongxl_p400_32_kernel49" "mesongxl_p400_kernel49" "mesongxl_p401_32_kernel49" "mesongxl_p401_kernel49" "mesongxl_p212_32_kernel49" "mesongxl_p212_kernel49" "mesongxm_q200_32_kernel49" "mesongxm_q200_kernel49" "meson8b_m200_kernel49" "mesongxl_p400" "mesongxl_p400_32" "mesongxl_p401" "mesongxl_p401_32" "mesongxb_p200" "mesongxb_p200_32" "mesongxb_p201" "mesongxb_p201_32" "mesongxl_p212" "mesongxl_p212_32" "mesongxl_p230" "mesongxl_p230_32" "mesongxl_p231" "mesongxl_p231_32" "mesongxl_p241" "mesongxl_p241_32" "mesongxm_q200" "mesongxm_q200_32" "meson8_k200" "meson8_k200b" "meson8b_m200" "meson8b_m201" "meson8m2_n200")
 
 DEFCONFIG_ARRAY_LEN=${#DEFCONFIG_ARRAY[@]}
 
@@ -28,6 +26,46 @@ function choose_info()
 		let i++
 	done
 	echo
+}
+
+function get_index() {
+	if [ $# -eq 0 ]; then
+		return 0
+	fi
+
+	i=0
+	while [[ $i -lt $DEFCONFIG_ARRAY_LEN ]]
+	do
+		if [ $1 = "${DEFCONFIG_ARRAY[$i]}_release" ]; then
+			let i++
+			return ${i}
+		fi
+		let i++
+	done
+	return 0
+}
+
+function get_target_board_type() {
+	TARGET=$1
+	RESULT="$(echo $TARGET | cut -d '_' -f 2)"
+	echo "$RESULT"
+}
+
+function get_target_build_type() {
+	TARGET=$1
+	LENGTH="$(echo $TARGET | awk -F '_' '{print NF}')"
+	if [ $LENGTH -le 2 ]; then
+		echo "64"
+	else
+		RESULT="$(echo $TARGET | cut -d '_' -f 3)"
+		if [ $RESULT = "32" ]; then
+			echo "32"
+		elif [ $RESULT = "64" ]; then
+			echo "64"
+		else
+			echo "64"
+		fi
+	fi
 }
 
 function choose_type()
@@ -58,17 +96,27 @@ function choose_type()
 				index=$((${ANSWER}-1))
 				TARGET_BUILD_CONFIG="${DEFCONFIG_ARRAY[$index]}_release"
 				TARGET_DIR_NAME="${DEFCONFIG_ARRAY[$index]}"
-				TARGET_BUILD_TYPE="${BUILD_TYPE_ARRAY[$index]}"
-				TARGET_BOARD_TYPE="${BOARD_ARRAY[$index]}"
+				TARGET_BUILD_TYPE=`get_target_build_type ${DEFCONFIG_ARRAY[$index]}`
+				TARGET_BOARD_TYPE=`get_target_board_type ${DEFCONFIG_ARRAY[$index]}`
 			else
 				echo
 				echo "number not in range. Please try again."
 				echo
 			fi
 		else
-			echo
-			echo "I didn't understand your response.  Please try again."
-			echo
+			get_index $ANSWER
+			ANSWER=$?
+			if [ $ANSWER -gt 0 ]; then
+				index=$((${ANSWER}-1))
+				TARGET_BUILD_CONFIG="${DEFCONFIG_ARRAY[$index]}_release"
+				TARGET_DIR_NAME="${DEFCONFIG_ARRAY[$index]}"
+				TARGET_BUILD_TYPE=`get_target_build_type ${DEFCONFIG_ARRAY[$index]}`
+				TARGET_BOARD_TYPE=`get_target_board_type ${DEFCONFIG_ARRAY[$index]}`
+			else
+				echo
+				echo "I didn't understand your response.  Please try again."
+				echo
+			fi
 		fi
 		if [ -n "$1" ]; then
 			break
@@ -96,7 +144,7 @@ function lunch()
 }
 function function_stuff()
 {
-    choose_type
-    lunch
+	choose_type $@
+	lunch
 }
-function_stuff
+function_stuff $@
