0567a27081a19b19b20e584aca44be00127757b3
Yalong Liu <yalong.liu@amlogic.com>
2017-06-08 16:02:06 +0800

    PD#141291: Enable 32bit chromium on p212
    Change-Id: I0010e494cdcb5db167e9b0b5ae575655e60c8fad

---
+++ package/chromium/chromium-prebuilt/chromium-prebuilt.mk
@@ -0,0 +1,40 @@
+#############################################################
+#
+# Chromium-prebuilt
+#
+#############################################################
+ifeq ($(BR2_PACKAGE_CHROMIUM_PREBUILT),y)
+
+CHROMIUM_PREBUILT_VERSION = 53.0.2785.143
+
+CHROMIUM_PREBUILT_DEPENDENCIES = libxkbcommon gconf libexif cups libnss libdrm pciutils pulseaudio krb5 pango
+
+#prebuilt defines.
+CHROMIUM_PREBUILT_SITE = $(TOPDIR)/../vendor/amlogic/chromium
+CHROMIUM_PREBUILT_SITE_METHOD = local
+
+ifeq ($(BR2_aarch64),y)
+CHROMIUM_PREBUILT_DIRECTORY = vendor/amlogic/chromium/chromium-$(CHROMIUM_PREBUILT_VERSION)/arm64
+else
+CHROMIUM_PREBUILT_DIRECTORY = vendor/amlogic/chromium/chromium-$(CHROMIUM_PREBUILT_VERSION)/arm
+endif
+
+define CHROMIUM_PREBUILT_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/bin/chromium-browser
+	cp -a $(TOPDIR)/../$(CHROMIUM_PREBUILT_DIRECTORY)/chrome            $(TARGET_DIR)/usr/bin/chromium-browser
+	cp -a $(TOPDIR)/../$(CHROMIUM_PREBUILT_DIRECTORY)/*.pak             $(TARGET_DIR)/usr/bin/chromium-browser
+	cp -a $(TOPDIR)/../$(CHROMIUM_PREBUILT_DIRECTORY)/resources         $(TARGET_DIR)/usr/bin/chromium-browser
+	cp -a $(TOPDIR)/../$(CHROMIUM_PREBUILT_DIRECTORY)/locales           $(TARGET_DIR)/usr/bin/chromium-browser
+	cp -a $(TOPDIR)/../$(CHROMIUM_PREBUILT_DIRECTORY)/icudtl.dat        $(TARGET_DIR)/usr/bin/chromium-browser
+endef
+
+define CHROMIUM_PREBUILT_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/chromium/S90chrome \
+		$(TARGET_DIR)/etc/init.d/S90chrome
+	$(INSTALL) -D -m 755 package/chromium/amlogic.html \
+		$(TARGET_DIR)/var/www/amlogic.html
+endef
+
+$(eval $(generic-package))
+
+endif
--- package/chromium/chromium.mk
+++ package/chromium/chromium.mk
@@ -3,15 +3,22 @@
 # Chromium
 #
 #############################################################
+ifeq ($(BR2_PACKAGE_CHROMIUM_PREBUILT),y)
+include package/chromium/chromium-prebuilt/chromium-prebuilt.mk
+endif
+
+
+ifeq ($(BR2_PACKAGE_CHROMIUM_COMPILE_ALL),y)
 
 CHROMIUM_VERSION = 53.0.2785.143
-CHROMIUM_SOURCE = chromium-$(CHROMIUM_VERSION).tar.gz
-# This URL will not work directly, it is assumed the chromium tarball is already placed in buildroot/dl
-CHROMIUM_SITE = http://openlinux.amlogic.com:8000/download/GPL_code_release/ThirdParty
 
 #CHROMIUM_LICENSE = GPLv3+
 #CHROMIUM_LICENSE_FILES = COPYING
 CHROMIUM_DEPENDENCIES = libxkbcommon gconf libexif cups libnss libdrm pciutils pulseaudio krb5 pango
+
+CHROMIUM_SOURCE = chromium-$(CHROMIUM_VERSION).tar.gz
+# This URL will not work directly, it is assumed the chromium tarball is already placed in buildroot/dl
+CHROMIUM_SITE = http://openlinux.amlogic.com:8000/download/GPL_code_release/ThirdParty
 
 CHROMIUM_FLAGS = 'disable_fatal_linker_warnings=1 v8_use_external_startup_data=0 use_allocator=none angle_use_commit_id=0 disable_nacl=1 use_kerberos=0 use_cups=0 use_gnome_keyring=0 linux_link_gnome_keyring=0 linux_link_kerberos=0 v8_use_snapshot="true" use_system_bzip=1 host_clang=0 clang=0 use_sysroot=1 component=static_library linux_use_bundled_gold=0 linux_use_bundled_binutils=0 linux_use_gold_flags=0 use_ozone=1 ozone_auto_platforms=1 ozone_platform_wayland=1 ozone_platform_gbm=0 use_xkbcommon=1 ffmpeg_branding=Chrome proprietary_codecs=1 enable_hevc_demuxing=1 enable_ac3_eac3_audio_demuxing=1 remove_webcore_debug_symbols=1 target_os=linux target_arch=arm arm_float_abi=hard target_sysroot=$(STAGING_DIR)'
 
@@ -33,4 +40,13 @@ define CHROMIUM_INSTALL_TARGET_CMDS
 	cp -a $(CHROMIUM_DIR)/src/out/$(CHROMIUM_MODE)/icudtl.dat        $(TARGET_DIR)/usr/bin/chromium-browser
 endef
 
+define CHROMIUM_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/chromium/S90chrome \
+		$(TARGET_DIR)/etc/init.d/S90chrome
+	$(INSTALL) -D -m 755 package/chromium/amlogic.html \
+		$(TARGET_DIR)/var/www/amlogic.html
+endef
+
 $(eval $(generic-package))
+
+endif
