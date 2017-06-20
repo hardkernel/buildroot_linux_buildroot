67bdd9f514bf7e256c3bbb947ee64948a61f3ddb
Haibing.An <haibing.an@amlogic.com>
2017-06-20 19:07:05 +0800

    change the directory that the depend libraries of alsaplayer,include libogg libvorbis
    Change-Id: Iaeda42103cd15e922d11c34c8c71e710a220bf1c

--- package/alsaplayer/alsaplayer.mk
+++ package/alsaplayer/alsaplayer.mk
@@ -9,8 +9,8 @@ ALSAPLAYER_SOURCE = alsaplayer-$(ALSAPLAYER_VERSION).tar.bz2
 ALSAPLAYER_VERSION = 0.99.81
 ALSAPLAYER_CONF_OPTS = --enable-esd=no \
 					   --enable-oss=no \
-					   --with-ogg-prefix=${TARGET_OUTPUT_DIR}/host/usr/aarch64-buildroot-linux-gnu/sysroot/usr/lib \
-					   --with-vorbis-prefix=${TARGET_OUTPUT_DIR}host/usr/aarch64-buildroot-linux-gnu/sysroot/usr/lib
+					   --with-ogg-prefix=${STAGING_DIR}/usr/lib \
+					   --with-vorbis-prefix=${STAGING_DIR}/usr/lib
 ALSAPLAYER_AUTORECONF = YES
 ALSAPLAYER_DEPENDENCIES = host-pkgconf libglib2 libid3tag libmad libogg flac libvorbis libsndfile
 ALSAPLAYER_LICENSE = GPLv2+
