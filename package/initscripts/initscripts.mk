c09712f5803f4823b5141881ce3a5034e179d994
Renjun Xu <renjun.xu@amlogic.com>
2017-07-11 15:52:11 +0800

    add DirectFB directfbrc config file and modified qt test demo for LCD display
    Change-Id: Ib6e013cb673cbe1bb694cee0c18a4969aca91024

--- package/initscripts/initscripts.mk
+++ package/initscripts/initscripts.mk
@@ -7,6 +7,7 @@
 define INITSCRIPTS_INSTALL_TARGET_CMDS
 	mkdir -p  $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -D -m 0755 package/initscripts/init.d/* $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 0755 package/initscripts/etc/* $(TARGET_DIR)/etc/
 endef
 
 $(eval $(generic-package))
