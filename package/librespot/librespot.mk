6127ff8da428b927b6fece8566e46e075c7e2d51
maya <zhenhua.he@amlogic.com>
2017-07-27 14:21:27 +0800

    add spotify connector
    Change-Id: Id2ae4cd0164bbf5411761205cf829e448900dbf5
    Signed-off-by: zhenhua he <zhenhua.he@amlogic.com>

---
+++ package/librespot/librespot.mk
@@ -0,0 +1,45 @@
+####################################################################################
+#
+#librespot
+#
+####################################################################################
+
+LIBRESPOT_VERSION = 0.1
+LIBRESPOT_SOURCE = librespot.tar.gz
+LIBRESPOT_SITE_METHOD = local
+LIBRESPOT_LICENSE = MIT LICENSE
+LIBRESPOT_LICENSE_FILES = LICENSE
+LIBRESPOT_STAGING = YES
+
+SYSROOT_CONFIG = $(@D)/../../host/usr/bin/gcc-sysroot
+
+
+
+define LIBRESPOT_INSTALL_TARGET_CMDS
+
+	curl -OL http://mirrordirector.raspbian.org/raspbian/pool/main/a/alsa-lib/libasound2_1.0.25-4_armhf.deb
+	curl -OL http://mirrordirector.raspbian.org/raspbian/pool/main/a/alsa-lib/libasound2-dev_1.0.25-4_armhf.deb
+	ar p $(TOPDIR)/libasound2_1.0.25-4_armhf.deb data.tar.gz | tar -xz -C  $(STAGING_DIR)
+	ar p $(TOPDIR)/libasound2-dev_1.0.25-4_armhf.deb data.tar.gz | tar -xz -C $(STAGING_DIR)
+	rm $(TOPDIR)/libasound2-dev_1.0.25-4_armhf.deb
+	rm $(TOPDIR)/libasound2_1.0.25-4_armhf.deb
+
+
+
+	echo -e '#!/bin/bash' > $(SYSROOT_CONFIG)
+	echo  "arm-linux-gnueabihf-gcc --sysroot $(STAGING_DIR) \"\$$@\"" >> $(SYSROOT_CONFIG)
+
+
+	chmod +x $(SYSROOT_CONFIG)
+	mkdir -p ~/.cargo
+	echo -e '[target.armv7-unknown-linux-gnueabihf]\nlinker = "$(SYSROOT_CONFIG)"' > $(@D)/.cargo/config
+	cp $(@D)/.cargo ~/ -rf
+	$(@D)/.cargo/bin/rustup target add armv7-unknown-linux-gnueabihf
+	$(@D)/.cargo/bin/cargo build --no-default-features --features "alsa-backend" --target armv7-unknown-linux-gnueabihf --release --manifest-path $(@D)/Cargo.toml
+
+
+	${INSTALL} -D -m 0755 ${@D}/target/armv7-unknown-linux-gnueabihf/release/librespot  ${TARGET_DIR}/usr/bin/librespot
+	rm ~/.cargo -rf
+endef
+
+$(eval $(generic-package))
