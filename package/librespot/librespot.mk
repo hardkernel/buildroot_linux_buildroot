c91fccf373f07bacc09156a4b667a1cbe184f6b6
maya <zhenhua.he@amlogic.com>
2017-07-28 17:30:25 +0800

    PD#148357 ：add spotify connector librespot
    Change-Id: Id28492e6fe1c9fe8a96c12599f61925889999155

--- package/librespot/librespot.mk
+++ package/librespot/librespot.mk
@@ -6,13 +6,12 @@
 
 LIBRESPOT_VERSION = 0.1
 LIBRESPOT_SOURCE = librespot.tar.gz
-LIBRESPOT_SITE_METHOD = local
 LIBRESPOT_LICENSE = MIT LICENSE
 LIBRESPOT_LICENSE_FILES = LICENSE
 LIBRESPOT_STAGING = YES
+LIBRESPOT_SITE = http://openlinux.amlogic.com:8000/download/GPL_code_release/ThirdParty
 
 SYSROOT_CONFIG = $(@D)/../../host/usr/bin/gcc-sysroot
-
 
 
 define LIBRESPOT_INSTALL_TARGET_CMDS
@@ -24,8 +23,6 @@ define LIBRESPOT_INSTALL_TARGET_CMDS
 	rm $(TOPDIR)/libasound2-dev_1.0.25-4_armhf.deb
 	rm $(TOPDIR)/libasound2_1.0.25-4_armhf.deb
 
-
-
 	echo -e '#!/bin/bash' > $(SYSROOT_CONFIG)
 	echo  "arm-linux-gnueabihf-gcc --sysroot $(STAGING_DIR) \"\$$@\"" >> $(SYSROOT_CONFIG)
 
@@ -34,6 +31,7 @@ define LIBRESPOT_INSTALL_TARGET_CMDS
 	mkdir -p ~/.cargo
 	echo -e '[target.armv7-unknown-linux-gnueabihf]\nlinker = "$(SYSROOT_CONFIG)"' > $(@D)/.cargo/config
 	cp $(@D)/.cargo ~/ -rf
+	$(@D)/.cargo/bin/rustup default nightly
 	$(@D)/.cargo/bin/rustup target add armv7-unknown-linux-gnueabihf
 	$(@D)/.cargo/bin/cargo build --no-default-features --features "alsa-backend" --target armv7-unknown-linux-gnueabihf --release --manifest-path $(@D)/Cargo.toml
 
