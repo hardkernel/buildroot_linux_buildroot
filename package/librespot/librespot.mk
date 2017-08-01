ed698b9388913789d79bdf5a37dacba35ae73ee4
maya <zhenhua.he@amlogic.com>
2017-08-01 16:21:40 +0800

    PD#148475 update spotify connector and update alsa-lib to staging
    Change-Id: Id083757bd587917af90bc1a7f0318fc675026625

--- package/librespot/librespot.mk
+++ package/librespot/librespot.mk
@@ -4,40 +4,34 @@
 #
 ####################################################################################
 
-LIBRESPOT_VERSION = 0.1
+LIBRESPOT_VERSION = 0.2
 LIBRESPOT_SOURCE = librespot.tar.gz
 LIBRESPOT_LICENSE = MIT LICENSE
 LIBRESPOT_LICENSE_FILES = LICENSE
 LIBRESPOT_STAGING = YES
 LIBRESPOT_SITE = http://openlinux.amlogic.com:8000/download/GPL_code_release/ThirdParty
-
+LIBRESPOT_DEPENDENCIES = alsa-lib
 SYSROOT_CONFIG = $(@D)/../../host/usr/bin/gcc-sysroot
 
+ifeq ($(BR2_aarch64),y)
+RUST_CC = aarch64-unknown-linux-gnu
+else
+RUST_CC = armv7-unknown-linux-gnueabihf
+endif
 
 define LIBRESPOT_INSTALL_TARGET_CMDS
 
-	curl -OL http://mirrordirector.raspbian.org/raspbian/pool/main/a/alsa-lib/libasound2_1.0.25-4_armhf.deb
-	curl -OL http://mirrordirector.raspbian.org/raspbian/pool/main/a/alsa-lib/libasound2-dev_1.0.25-4_armhf.deb
-	ar p $(TOPDIR)/libasound2_1.0.25-4_armhf.deb data.tar.gz | tar -xz -C  $(STAGING_DIR)
-	ar p $(TOPDIR)/libasound2-dev_1.0.25-4_armhf.deb data.tar.gz | tar -xz -C $(STAGING_DIR)
-	rm $(TOPDIR)/libasound2-dev_1.0.25-4_armhf.deb
-	rm $(TOPDIR)/libasound2_1.0.25-4_armhf.deb
-
 	echo -e '#!/bin/bash' > $(SYSROOT_CONFIG)
-	echo  "arm-linux-gnueabihf-gcc --sysroot $(STAGING_DIR) \"\$$@\"" >> $(SYSROOT_CONFIG)
-
-
+	echo  "$(TARGET_CC) --sysroot $(STAGING_DIR) \"\$$@\"" >> $(SYSROOT_CONFIG)
 	chmod +x $(SYSROOT_CONFIG)
 	mkdir -p ~/.cargo
-	echo -e '[target.armv7-unknown-linux-gnueabihf]\nlinker = "$(SYSROOT_CONFIG)"' > $(@D)/.cargo/config
+	echo -e '[target.$(RUST_CC)]\nlinker = "$(SYSROOT_CONFIG)"' > $(@D)/.cargo/config
 	cp $(@D)/.cargo ~/ -rf
 	$(@D)/.cargo/bin/rustup default nightly
-	$(@D)/.cargo/bin/rustup target add armv7-unknown-linux-gnueabihf
-	$(@D)/.cargo/bin/cargo build --no-default-features --features "alsa-backend" --target armv7-unknown-linux-gnueabihf --release --manifest-path $(@D)/Cargo.toml
-
-
-	${INSTALL} -D -m 0755 ${@D}/target/armv7-unknown-linux-gnueabihf/release/librespot  ${TARGET_DIR}/usr/bin/librespot
-	rm ~/.cargo -rf
+	$(@D)/.cargo/bin/rustup target add $(RUST_CC)
+	$(@D)/.cargo/bin/cargo build --no-default-features --features "alsa-backend" --target $(RUST_CC)  --release --manifest-path $(@D)/Cargo.toml
+	${INSTALL} -D -m 0755 ${@D}/target/$(RUST_CC)/release/librespot  ${TARGET_DIR}/usr/bin/librespot
+	#rm ~/.cargo -rf
 endef
 
 $(eval $(generic-package))
