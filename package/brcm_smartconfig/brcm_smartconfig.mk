c02b5d0ac95a7dc4c162f8c7b0e4ce6cd8c32890
Weiguang Ruan <Weiguang.ruan@amlogic.com>
2017-09-22 18:03:28 +0800

    buildroot: add broadcom smartconfig
    PD#151616
    
    Change-Id: I1a557d61accffc1ff40521a213e07ef4d0dcd48a

---
+++ package/brcm_smartconfig/brcm_smartconfig.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# bcm_smartconfig
+#
+################################################################################
+
+BRCM_SMARTCONFIG_VERSION = 3.70
+BRCM_SMARTCONFIG_SITE = $(call qstrip,$(BR2_PACKAGE_BRCM_SMARTCONFIG_LOCAL_PATH))
+BRCM_SMARTCONFIG_SITE_METHOD = local
+
+
+define BRCM_SMARTCONFIG_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/jni CC=$(TARGET_CC)
+endef
+
+define BRCM_SMARTCONFIG_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/jni/setup $(TARGET_DIR)/usr/bin/brcm_smartconfig
+	$(INSTALL) -D -m 755 package/brcm_smartconfig/brcm_smartconfig.sh $(TARGET_DIR)/usr/bin
+endef
+
+
+$(eval $(generic-package))
