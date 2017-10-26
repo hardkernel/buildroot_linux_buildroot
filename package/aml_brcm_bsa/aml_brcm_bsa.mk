d84d2180b9ee8995720f6f573dc814026e295d0c
Peipeng Zhao <peipeng.zhao@amlogic.com>
2017-10-26 18:55:40 +0800

    Bluetooth: add handfree profile function and dspc library
    PD#153130: add handfree function with libdspc
    
    Change-Id: Ic827de359275b3b62693829a547c049ed10c6560
    Signed-off-by: Peipeng Zhao <peipeng.zhao@amlogic.com>

--- package/aml_brcm_bsa/aml_brcm_bsa.mk
+++ package/aml_brcm_bsa/aml_brcm_bsa.mk
@@ -7,10 +7,12 @@ AML_BRCM_BSA_SITE_METHOD = local
 AML_BRCM_BSA_PATH = 3rdparty/embedded/bsa_examples/linux
 AML_BRCM_BSA_LIBBSA = libbsa
 
+AML_BT_DSPC_PATH = 3rdparty/embedded/bsa_examples/linux
+AML_BT_DSPC_LIBDSPC = libdspc
 ###if mem reductin configured, only support few profils
 ifeq ($(BR2_AML_BRCM_BSA_MEM_REDUCTION),y)
 AML_BRCM_BSA_APP = app_manager app_ag app_avk app_hs app_av \
-	app_switch app_tm app_socket app_musicBox
+	app_switch app_tm app_socket
 	TARGET_CONFIGURE_OPTS += CONFIG_MEM_REDUCTION=y
 define AML_BRCM_BSA_INSTALL_SERVER_SO_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/server/$(AML_BRCM_BSA_BUILD_TYPE)_mem_reduce/bsa_server \
@@ -19,12 +21,16 @@ define AML_BRCM_BSA_INSTALL_SERVER_SO_TARGET_CMDS
 		$(TARGET_DIR)/usr/lib/libbsa.so
 endef
 else
-AML_BRCM_BSA_APP = app_manager app_3d app_ag app_av app_avk app_ble \
+#AML_BRCM_BSA_APP = app_manager app_3d app_ag app_av app_avk app_ble \
 	app_ble_ancs app_ble_blp app_ble_cscc app_ble_eddystone app_ble_hrc \
 	app_ble_htp app_ble_pm app_ble_rscc app_ble_tvselect app_ble_wifi \
 	app_cce app_dg app_fm app_ftc app_fts app_hd app_headless \
 	app_hh app_hl app_hs app_nsa app_opc app_ops app_pan \
-	app_pbc app_pbs app_sac app_sc app_switch app_tm app_socket app_musicBox
+	app_pbc app_pbs app_sac app_sc app_switch app_tm app_socket
+
+AML_BRCM_BSA_APP = app_manager app_ag app_av app_avk \
+	app_hs \
+	app_switch app_tm
 
 ##if qt5 configured, we can use bsa qt app####
 ifeq ($(BR2_PACKAGE_QT5),y)
@@ -44,6 +50,7 @@ define AML_BRCM_BSA_INSTALL_TARGET_CMDS_QT
 endef
 
 endif
+AML_BRCM_MUSIC_BOX = app_musicBox
 #######qt app configure end###################
 
 define AML_BRCM_BSA_INSTALL_SERVER_SO_TARGET_CMDS
@@ -65,27 +72,79 @@ else
 AML_BRCM_BSA_BUILD_TYPE = arm
 endif
 
-
-
+ifeq ($(BR2_AML_BT_DSPC),y)
 define AML_BRCM_BSA_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$(AML_BRCM_BSA_PATH)/$(AML_BRCM_BSA_LIBBSA)/build \
-		CPU=$(AML_BRCM_BSA_BUILD_TYPE) ARMGCC=$(TARGET_CC)
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$(AML_BT_DSPC_PATH)/$(AML_BT_DSPC_LIBDSPC)/ \
+		CC="$(EXTERNAL_TOOLCHAIN_DIR)/bin/arm-linux-gnueabihf-g++" \
+		CFLAGS="-Wall -O3 -std=c++0x -fpic -D_GLIBCXX_USE_NANOSLEEP  -I$(STAGING_DIR)/usr/include" \
+		LDFLAGS="-lpthread -L$(TARGET_DIR)/usr/lib32  -lasound -L./lib -lAWELib"
+###	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$(AML_BRCM_BSA_PATH)/$(AML_BRCM_BSA_LIBBSA)/build \
+###		CPU=$(AML_BRCM_BSA_BUILD_TYPE) ARMGCC=$(TARGET_CC)
 	for ff in $(AML_BRCM_BSA_APP); do \
 		$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$(AML_BRCM_BSA_PATH)/$$ff/build \
 			CPU=$(AML_BRCM_BSA_BUILD_TYPE) ARMGCC=$(TARGET_CC) BSASHAREDLIB=TRUE; \
 	done
+	$(TARGET_CONFIGURE_OPTS) $(MAKE)  -C $(@D)/$(AML_BRCM_BSA_PATH)/$(AML_BRCM_MUSIC_BOX)/build \
+		CPU=$(AML_BRCM_BSA_BUILD_TYPE) ARMGCC=$(TARGET_CC) ENABLE_ALSA_DSPC=TRUE BSASHAREDLIB=TRUE;
 
-	$(AML_BRCM_BSA_BUILD_CMDS_QT)
+###	$(AML_BRCM_BSA_BUILD_CMDS_QT)
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/3rdparty/embedded/brcm/linux/bthid ARCH=$(KERNEL_ARCH) \
+		CROSS_COMPILE=$(TARGET_KERNEL_CROSS) modules
+
+endef
+else
+define AML_BRCM_BSA_BUILD_CMDS
+##	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$(AML_BRCM_BSA_PATH)/$(AML_BRCM_BSA_LIBBSA)/build \
+##		CPU=$(AML_BRCM_BSA_BUILD_TYPE) ARMGCC=$(TARGET_CC)
+	for ff in $(AML_BRCM_BSA_APP); do \
+		$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$(AML_BRCM_BSA_PATH)/$$ff/build \
+			CPU=$(AML_BRCM_BSA_BUILD_TYPE) ARMGCC=$(TARGET_CC) BSASHAREDLIB=TRUE; \
+	done
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$(AML_BRCM_BSA_PATH)/$(AML_BRCM_MUSIC_BOX)/build \
+		CPU=$(AML_BRCM_BSA_BUILD_TYPE) ARMGCC=$(TARGET_CC) BSASHAREDLIB=TRUE;
+###	$(AML_BRCM_BSA_BUILD_CMDS_QT)
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/3rdparty/embedded/brcm/linux/bthid ARCH=$(KERNEL_ARCH) \
 		CROSS_COMPILE=$(TARGET_KERNEL_CROSS) modules
 
 endef
 
+endif
+
+ifeq ($(BR2_AML_BT_DSPC),y)
 define AML_BRCM_BSA_INSTALL_TARGET_CMDS
 	$(AML_BRCM_BSA_INSTALL_SERVER_SO_TARGET_CMDS)
 	for ff in $(AML_BRCM_BSA_APP); do \
 		$(INSTALL) -D -m 755 $(@D)/$(AML_BRCM_BSA_PATH)/$${ff}/build/$(AML_BRCM_BSA_BUILD_TYPE)/$${ff} $(TARGET_DIR)/usr/bin/${ff}; \
 	done
+
+
+	$(INSTALL) -D -m 755 $(@D)/$(AML_BRCM_BSA_PATH)/$(AML_BRCM_MUSIC_BOX)/build/$(AML_BRCM_BSA_BUILD_TYPE)/$(AML_BRCM_MUSIC_BOX) $(TARGET_DIR)/usr/bin/$(AML_BRCM_MUSIC_BOX)
+
+	$(AML_BRCM_BSA_INSTALL_TARGET_CMDS_QT)
+	mkdir -p $(TARGET_DIR)/etc/bsa
+	mkdir -p $(TARGET_DIR)/etc/bsa/config
+	$(INSTALL) -D -m 755 $(@D)/test_files/av/44k8bpsStereo.wav $(TARGET_DIR)/etc/bsa
+	$(INSTALL) -D -m 755 $(@D)/test_files/dg/tx_test_file.txt $(TARGET_DIR)/etc/bsa
+	$(INSTALL) -D -m 755 package/aml_brcm_bsa/S44bluetooth $(TARGET_DIR)/etc/init.d
+
+	$(INSTALL) -D -m 755 $(@D)/$(AML_BT_DSPC_PATH)/$(AML_BT_DSPC_LIBDSPC)/libdspc.so \
+		$(TARGET_DIR)/usr/lib/libdspc.so
+	$(INSTALL) -D -m 755 $(@D)/$(AML_BT_DSPC_PATH)/$(AML_BT_DSPC_LIBDSPC)/AMLogic_VUI_Solution_v6a_VoiceOnly_release.awb  \
+		$(TARGET_DIR)/etc/bsa/config/AMLogic_VUI_Solution_v6a_VoiceOnly_release.awb
+	$(INSTALL) -D -m 755 $(@D)/$(AML_BT_DSPC_PATH)/$(AML_BT_DSPC_LIBDSPC)/netfile.raw \
+		$(TARGET_DIR)/etc/bsa/config/netfile.raw
+	$(INSTALL) -D -m 755 $(@D)/$(AML_BT_DSPC_PATH)/$(AML_BT_DSPC_LIBDSPC)/searchfile.raw \
+		$(TARGET_DIR)/etc/bsa/config/searchfile.raw
+endef
+else
+define AML_BRCM_BSA_INSTALL_TARGET_CMDS
+	$(AML_BRCM_BSA_INSTALL_SERVER_SO_TARGET_CMDS)
+	for ff in $(AML_BRCM_BSA_APP); do \
+		$(INSTALL) -D -m 755 $(@D)/$(AML_BRCM_BSA_PATH)/$${ff}/build/$(AML_BRCM_BSA_BUILD_TYPE)/$${ff} $(TARGET_DIR)/usr/bin/${ff}; \
+	done
+
+
+	$(INSTALL) -D -m 755 $(@D)/$(AML_BRCM_BSA_PATH)/$(AML_BRCM_MUSIC_BOX)/build/$(AML_BRCM_BSA_BUILD_TYPE)/$(AML_BRCM_MUSIC_BOX) $(TARGET_DIR)/usr/bin/$(AML_BRCM_MUSIC_BOX)
 
 	$(AML_BRCM_BSA_INSTALL_TARGET_CMDS_QT)
 	mkdir -p $(TARGET_DIR)/etc/bsa
@@ -95,6 +154,5 @@ define AML_BRCM_BSA_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 package/aml_brcm_bsa/S44bluetooth $(TARGET_DIR)/etc/init.d
 
 endef
-
-
+endif
 $(eval $(generic-package))
