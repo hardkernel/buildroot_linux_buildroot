982044b989a0ba7714f3f8c070560cecefaf5d74
Zhigang Yu <zhigang.yu@amlogic.com>
2017-10-30 15:09:52 +0800

    recovery: add dtb.img to package for upgrade
    PD#150940: add dtb.img to software.swu for upgrade
    
    Change-Id: Iba5549564caa55cadb51e0120ce86e57ef587b91

--- board/amlogic/common/ota/ota-axg/ota_package_create.sh
+++ board/amlogic/common/ota/ota-axg/ota_package_create.sh
@@ -3,7 +3,7 @@ CONTAINER_VER="1.0"
 PRODUCT_NAME="aml-software"
 cd ${BINARIES_DIR}
 
-HASH_FILES="u-boot.bin.usb.bl2 u-boot.bin.usb.tpl boot.img rootfs.ubifs update.sh"
+HASH_FILES="u-boot.bin.usb.bl2 u-boot.bin.usb.tpl dtb.img boot.img rootfs.ubifs update.sh"
 for i in $HASH_FILES;do
 	value_tmp=$(sha256sum $i);
 	value_sha256=${value_tmp:0:64};
@@ -12,7 +12,7 @@ for i in $HASH_FILES;do
 done
 
 
-FILES="sw-description sw-description.sig u-boot.bin.usb.bl2 u-boot.bin.usb.tpl boot.img rootfs.ubifs update.sh"
+FILES="sw-description sw-description.sig u-boot.bin.usb.bl2 u-boot.bin.usb.tpl dtb.img boot.img rootfs.ubifs update.sh"
 
 
 openssl dgst -sha256 -sign swupdate-priv.pem sw-description > sw-description.sig
