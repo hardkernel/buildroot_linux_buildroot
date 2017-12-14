4c2f218153aa875ecba71911e65fa139219ddb2e
Peipeng Zhao <peipeng.zhao@amlogic.com>
2017-12-14 15:26:18 +0800

    PD#156863: bluetooth: add bluetooth to setup wifi function
    Bluetooth: add app_ble profile for setup wifi
    
    Change-Id: I0f0fa7d00a01885573bbac71a918a5e230bfa216
    Signed-off-by: Peipeng Zhao <peipeng.zhao@amlogic.com>

--- board/amlogic/mesonaxg_s400/rootfs/etc/adckey/adckey_function.sh
+++ board/amlogic/mesonaxg_s400/rootfs/etc/adckey/adckey_function.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+NAME1=app_ble_wifi_setup
+DAEMON1=/usr/bin/$NAME1
+PIDFILE1=/var/run/$NAME1.pid
+
+NAME2=app_musicBox
+DAEMON2=/usr/bin/$NAME2
+PIDFILE2=/var/run/$NAME2.pid
+
 powerStateFile="/sys/power/state"
 powerResumeFlag="/etc/adckey/powerState"
 
@@ -42,11 +50,26 @@ wifiSmartConfig()
 	brcm_smartconfig.sh
 }
 
+ble_wifi_setup()
+{
+	echo "ble config for wifisetup"
+	rm /etc/bsa/config/wifi_tool.sh
+	ln /var/www/cgi-bin/wifi/wifi_tool.sh  /etc/bsa/config/wifi_tool.sh
+	local app1_id=`ps | grep "app_musicBox" | awk '{print $1}'`
+	kill -9 $app1_id
+	local app2_id=`ps | grep "app_ble_wifi_setup" | awk '{print $1}'`
+	kill -9 $app2_id
+	cd /etc/bsa/config
+        app_ble_wifi_setup &
+	app_musicBox  ble_mode &
+}
+
 case $1 in
     "power") powerStateChange ;;
     "VolumeUp") volumeUpAction ;;
     "VolumeDown") volumeDownAction ;;
     "longpressWifiConfig") wifiSmartConfig ;;
+    "WifiConfig")  ble_wifi_setup ;;
     *) echo "no function to add this case: $1" ;;
 esac
 
