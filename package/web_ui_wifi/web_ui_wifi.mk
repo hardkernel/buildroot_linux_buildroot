1c6cb20b5722126bbdedea3a6ac9d59175eaf32f
Yueguie He <yuegui.he@amlogic.com>
2018-01-15 11:38:29 +0800

    PD#158625: update web_ui_wifi makefile to fix rename TMP_NAME varible
    Change-Id: If27a42f6669b4b47493fe55c7f04da42ebcd6167
    Signed-off-by: Yueguie He <yuegui.he@amlogic.com>

--- package/web_ui_wifi/web_ui_wifi.mk
+++ package/web_ui_wifi/web_ui_wifi.mk
@@ -9,16 +9,16 @@ WEB_UI_WIFI_SITE_METHOD = local
 WEB_UI_WIFI_SITE = ${TOPDIR}/package/web_ui_wifi/src
 
 ifeq ($(BR2_aarch64),y)
-	TMP_NAME = main64.cgi
+	MAIN_CGI_BIN = main64.cgi
 else
-	TMP_NAME = main32.cgi
+	MAIN_CGI_BIN = main32.cgi
 endif
 
 
 define WEB_UI_WIFI_INSTALL_TARGET_CMDS
 	mkdir -p ${TARGET_DIR}/var/www
 	cp $(@D)/Html/* ${TARGET_DIR}/var/www/
-	cp $(@D)/cgi-bin/${TMP_NAME} ${TARGET_DIR}/var/www/cgi-bin/main.cgi
+	cp $(@D)/cgi-bin/${MAIN_CGI_BIN} ${TARGET_DIR}/var/www/cgi-bin/main.cgi
 	cp -rf $(@D)/cgi-bin/scripts/* ${TARGET_DIR}/var/www/cgi-bin/
 endef
 
