4d642f68fabfe664d5fa0d65c79f709e0fb658d4
wenbiao zhang <wenbiao.zhang@amlogic.com>
2018-05-22 10:27:10 +0800

    PD#NONE: add opus audio support
    Change-Id: I0078a5bf676dc76f94b0c32acd3062f9ff23f113

--- board/amlogic/mesongxl_p212_kernel49/rootfs/etc/property_set.sh
+++ board/amlogic/mesongxl_p212_kernel49/rootfs/etc/property_set.sh
@@ -5,7 +5,7 @@ export LOG_LEVEL=0
 export CURLLOG_LEVEL=0
 export DASHLOG_LEVEL=0
 #audio formats to decode use arm decoder
-export media_arm_audio_decoder=ape,flac,dts,ac3,eac3,wma,wmapro,mp3,aac,aac_latm,vorbis,raac,cook,amr,pcm,adpcm
+export media_arm_audio_decoder=ape,flac,dts,ac3,eac3,wma,wmapro,mp3,aac,aac_latm,vorbis,opus,raac,cook,amr,pcm,adpcm
 #third party lib modules to use
 export media_libplayer_modules=libcurl_mod.so,libdash_mod.so,libvhls_mod.so
 
