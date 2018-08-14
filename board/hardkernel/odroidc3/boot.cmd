24a251af495114fcfee85f6c624ceb7fb2ea5f04
Dongjin Kim <tobetter@gmail.com>
2018-08-14 19:03:56 +0900

    ODROID-C3/N2: clone the board specific files from the AMLogic's reference
    Change-Id: Ia9744b9fe9b7079878fb88e7ba22ef2ada9454f9
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

---
+++ board/hardkernel/odroidc3/boot.cmd
@@ -0,0 +1,18 @@
+#
+# U-boot booting script
+#
+
+setenv bootargs "root=/dev/mmcblk0p2 rootfstype=ext4 rootwait init=/sbin/init console=ttyS0,115200 no_console_suspend earlyprintk=aml-uart,0xff803000 ramoops.pstore_en=1 ramoops.record_size=0x8000 ramoops.console_size=0x4000"
+
+setenv fdt_addr_r "0x1000000"
+setenv kernel_addr_r "0x1080000"
+setenv kernel_addr_load "0x3000000"
+setenv ramdisk_addr_r "0x3080000"
+
+setenv fdtfile "s905d2_odroidc3.dtb"
+
+fatload mmc ${devno}:1 ${kernel_addr_load} Image.gz \
+&& unzip ${kernel_addr_load} ${kernel_addr_r}
+&& fatload mmc ${devno}:1 ${fdt_addr_r} ${fdtfile} \
+&& if test -e mmc ${devno} initrd.gz; then fatload mmc ${devno}:1 ${ramdisk_addr_r} initrd.gz; else setenv ramdisk_addr_r "-"; fi;
+&& booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
---
+++ board/hardkernel/odroidn2/post-image.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+rm -rf "${GENIMAGE_TMP}"
+
+cp ${BOARD_DIR}/boot.cmd ${BINARIES_DIR}/
+
+echo "ODROIDN2-UBOOT-CONFIG" > ${BINARIES_DIR}/boot.ini
+cat ${BINARIES_DIR}/boot.cmd >> ${BINARIES_DIR}/boot.ini
+
+${HOST_DIR}/usr/bin/mkimage -A arm64 -T script -C none \
+	-d ${BINARIES_DIR}/boot.cmd ${BINARIES_DIR}/boot.scr
+
+genimage                           \
+	--rootpath "${TARGET_DIR}"     \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+dd if=${BINARIES_DIR}/u-boot.bin of=${BINARIES_DIR}/sdcard.img bs=512 seek=1 conv=fsync,notrunc
