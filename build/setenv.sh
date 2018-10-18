1ab476abf21959965fd2a2fc61a4fd678d82f9c9
Blance Tang <guofeng.tang@amlogic.com>
2018-10-18 16:33:03 +0800

    buildroot: Enable filter to setenv.sh
    PD#NONE setenv.sh [filter] will allow user see less option
    
    Signed-off-by: Blance Tang <guofeng.tang@amlogic.com>

--- build/setenv.sh
+++ build/setenv.sh
@@ -1,12 +1,94 @@
-#!/bin/sh
+#!/bin/bash
+
+#set -x
+#export PS4='+ [$(basename ${BASH_SOURCE})] [${LINENO}] '
 
 LOCAL_DIR=$(pwd)
 BUILDROOT_DIR=$LOCAL_DIR/buildroot
 BUILD_OUTPUT_DIR=$LOCAL_DIR/output
 
-DEFCONFIG_ARRAY=("mesong12b_skt_release" "mesong12b_w400_release" "mesong12b_w400_32_release" "mesong12a_skt_32_release" "mesong12a_skt_release" "mesong12a_u200_32_release" "mesong12a_u200_release" "mesong12a_u211_32_release" "mesong12a_u211_release" "txlx_t962e_r321_32_release" "txlx_t962e_r321_release" "mesonaxg_s400_32_release" "mesonaxg_s400_gva_32_release" "mesonaxg_s400_32_debug" "mesonaxg_s400_a3232_debug" "mesonaxg_s400_debug" "mesonaxg_s400_release" "mesonaxg_s400_32_emmc" "mesonaxg_s400_emmc"  "mesonaxg_s420_32_debug" "mesonaxg_s420_a3232_debug" "mesonaxg_s420_gva_32_release" "mesonaxg_s420_32_release" "mesonaxg_s420_debug" "mesonaxg_s420_release" "mesonaxg_a113x_skt_32"  "mesonaxg_a113x_skt" "mesonaxg_a113d_skt_32" "mesonaxg_a113d_skt" "mesongxl_p400_32_kernel49" "mesongxl_p400_kernel49" "mesongxl_p401_32_kernel49" "mesongxl_p401_kernel49" "mesongxl_p212_32_kernel49" "mesongxl_p212_kernel49" "mesongxl_p230_32_kernel49" "mesongxl_p230_kernel49" "mesongxl_p231_32_kernel49" "mesongxl_p231_kernel49" "mesongxl_p241_32_kernel49" "mesongxl_p241_kernel49" "mesongxm_q200_32_kernel49" "mesongxm_q200_kernel49" "meson8b_m200_kernel49" "mesongxl_p400" "mesongxl_p400_32" "mesongxl_p401" "mesongxl_p401_32" "mesongxb_p200" "mesongxb_p200_32" "mesongxb_p201" "mesongxb_p201_32" "mesongxl_p212" "mesongxl_p212_32" "mesongxl_p230" "mesongxl_p230_32" "mesongxl_p231" "mesongxl_p231_32" "mesongxl_p241" "mesongxl_p241_32" "mesongxm_q200" "mesongxm_q200_32" "meson8_k200" "meson8_k200b" "meson8b_m200" "meson8b_m201" "meson8m2_n200" "meson8b_m400")
+DEFCONFIG_ARRAY=(
+    "mesong12b_skt_release"
+    "mesong12b_w400_release"
+    "mesong12b_w400_32_release"
+    "mesong12a_skt_32_release"
+    "mesong12a_skt_release"
+    "mesong12a_u200_32_release"
+    "mesong12a_u200_release"
+    "mesong12a_u211_32_release"
+    "mesong12a_u211_release"
+    "txlx_t962e_r321_32_release"
+    "txlx_t962e_r321_release"
+    "mesonaxg_s400_32_release"
+    "mesonaxg_s400_gva_32_release"
+    "mesonaxg_s400_32_debug"
+    "mesonaxg_s400_a3232_debug"
+    "mesonaxg_s400_debug"
+    "mesonaxg_s400_release"
+    "mesonaxg_s400_32_emmc"
+    "mesonaxg_s400_emmc"
+    "mesonaxg_s420_32_debug"
+    "mesonaxg_s420_a3232_debug"
+    "mesonaxg_s420_gva_32_release"
+    "mesonaxg_s420_32_release"
+    "mesonaxg_s420_debug"
+    "mesonaxg_s420_release"
+    "mesonaxg_a113x_skt_32"
+    "mesonaxg_a113x_skt"
+    "mesonaxg_a113d_skt_32"
+    "mesonaxg_a113d_skt"
+    "mesongxl_p400_32_kernel49"
+    "mesongxl_p400_kernel49"
+    "mesongxl_p401_32_kernel49"
+    "mesongxl_p401_kernel49"
+    "mesongxl_p212_32_kernel49"
+    "mesongxl_p212_kernel49"
+    "mesongxl_p230_32_kernel49"
+    "mesongxl_p230_kernel49"
+    "mesongxl_p231_32_kernel49"
+    "mesongxl_p231_kernel49"
+    "mesongxl_p241_32_kernel49"
+    "mesongxl_p241_kernel49"
+    "mesongxm_q200_32_kernel49"
+    "mesongxm_q200_kernel49"
+    "meson8b_m200_kernel49"
+    "mesongxl_p400"
+    "mesongxl_p400_32"
+    "mesongxl_p401"
+    "mesongxl_p401_32"
+    "mesongxb_p200"
+    "mesongxb_p200_32"
+    "mesongxb_p201"
+    "mesongxb_p201_32"
+    "mesongxl_p212"
+    "mesongxl_p212_32"
+    "mesongxl_p230"
+    "mesongxl_p230_32"
+    "mesongxl_p231"
+    "mesongxl_p231_32"
+    "mesongxl_p241"
+    "mesongxl_p241_32"
+    "mesongxm_q200"
+    "mesongxm_q200_32"
+    "meson8_k200"
+    "meson8_k200b"
+    "meson8b_m200"
+    "meson8b_m201"
+    "meson8m2_n200"
+    "meson8b_m400")
 
 DEFCONFIG_ARRAY_LEN=${#DEFCONFIG_ARRAY[@]}
+
+FILTER=$1
+if [[ "$FILTER" =~ "_" ]]
+then
+    FILTER=""
+    DEFAULT_TARGET=$1
+else
+    DEFAULT_TARGET=""
+    FILTER=$1
+    echo "Enable manually build filter: =$FILTER="
+fi
 
 i=0
 while [[ $i -lt $DEFCONFIG_ARRAY_LEN ]]
@@ -16,23 +98,28 @@ done
 
 function choose_info()
 {
-	echo
-	echo "You're building on Linux"
-	echo "Lunch menu...pick a combo:"
-	i=0
-	while [[ $i -lt $DEFCONFIG_ARRAY_LEN ]]
-	do
-		if [[ ${DEFCONFIG_ARRAY[$i]} =~ "debug" ]]; then
-			echo "$((${i}+1)). ${DEFCONFIG_ARRAY[$i]}"
-		else if [[ ${DEFCONFIG_ARRAY[$i]} =~ "release" ]]; then
-			echo "$((${i}+1)). ${DEFCONFIG_ARRAY[$i]}"
-		else
-			echo "$((${i}+1)). ${DEFCONFIG_ARRAY[$i]}_release"
-		fi
-		fi
-		let i++
-	done
-	echo
+    echo
+    echo "You're building on Linux"
+    echo "Lunch menu...pick a combo:"
+    i=0
+    while [[ $i -lt $DEFCONFIG_ARRAY_LEN ]]
+    do
+        if [ -n "$FILTER" ] && [ -z "$(echo ${DEFCONFIG_ARRAY[$i]} | grep "$FILTER")" ]; then
+            #the build configuration did contain the specified string, hide it to user
+            let i++
+            continue
+        fi
+
+        if [[ ${DEFCONFIG_ARRAY[$i]} =~ "debug" ]]; then
+            echo "$((${i}+1)). ${DEFCONFIG_ARRAY[$i]}"
+        elif [[ ${DEFCONFIG_ARRAY[$i]} =~ "release" ]]; then
+            echo "$((${i}+1)). ${DEFCONFIG_ARRAY[$i]}"
+        else
+            echo "$((${i}+1)). ${DEFCONFIG_ARRAY[$i]}_release"
+        fi
+        let i++
+    done
+    echo
 }
 
 function get_index() {
@@ -119,24 +206,25 @@ function choose_type()
 {
 	choose_info
 	local DEFAULT_NUM DEFAULT_VALUE
-	DEFAULT_NUM=5
-	DEFAULT_VALUE="mesongxl_p212_release"
+	DEFAULT_NUM=-1
+	DEFAULT_VALUE="no-default-option"
 
 	export TARGET_BUILD_TYPE=
 	local ANSWER
+    local USER_DFT_OPT=$1
 	while [ -z $TARGET_BUILD_TYPE ]
 	do
 		echo -n "Which would you like? ["$DEFAULT_NUM"] "
-		if [ -z "$1" ]; then
+		if [ -z "$USER_DFT_OPT" ]; then
 			read ANSWER
 		else
-			echo $1
-			ANSWER=$1
+			echo $USER_DFT_OPT
+			ANSWER=$USER_DFT_OPT
 		fi
 
-		if [ -z "$ANSWER" ]; then
-			ANSWER="$DEFAULT_NUM"
-		fi
+#		if [ -z "$ANSWER" ]; then
+#			ANSWER="$DEFAULT_NUM"
+#		fi
 
 		if [ -n "`echo $ANSWER | sed -n '/^[0-9][0-9]*$/p'`" ]; then
 			if [ $ANSWER -le $DEFCONFIG_ARRAY_LEN ] && [ $ANSWER -gt 0 ]; then
@@ -163,9 +251,11 @@ function choose_type()
 				echo
 				echo "I didn't understand your response.  Please try again."
 				echo
+                USER_DFT_OPT=""
+#                choose_info
 			fi
 		fi
-		if [ -n "$1" ]; then
+		if [ -n "$USER_DFT_OPT" ]; then
 			break
 		fi
 	done
@@ -191,7 +281,7 @@ function lunch()
 }
 function function_stuff()
 {
-	choose_type $@
-	lunch
+    choose_type $@
+    lunch
 }
-function_stuff $@
+function_stuff $DEFAULT_TARGET
