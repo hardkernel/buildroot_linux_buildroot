012d45ed49ced1b777722139a61af4fc81bc0898
keke.li <keke.li@amlogic.com>
2018-10-19 17:30:43 +0800

    npu: add config for compile and install
    PD#NONE: add config for compile and install
    
    Change-Id: I94fc7efae7470d1cb4ab59f45bde51b083991d45
    Signed-off-by: Brian Zhu <brian.zhu@amlogic.com>

--- package/npu/npu.mk
+++ package/npu/npu.mk
@@ -9,9 +9,27 @@ NPU_SITE = $(call qstrip,$(BR2_PACKAGE_NPU_LOCAL_PATH))
 NPU_SITE_METHOD = local
 $(info ======>>>>>npu set compile path)
 $(info $(NPU_SITE))
-NPU_KO_INSTALL_DIR=$(TARGET_DIR)/etc/npu
+ARM_NPU_MODULE_DIR = kernel/amlogic/npu
+NPU_KO_INSTALL_DIR=$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/amlogic/npu
 NPU_SO_INSTALL_DIR=$(TARGET_DIR)/lib64
 NPU_DEPENDENCIES = linux
+
+ARM_NPU_DEP = $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep
+
+define copy-arm-npu
+        $(foreach m, $(shell find $(strip $(1)) -name "*.ko"),\
+                $(shell [ ! -e $(2) ] && mkdir $(2) -p;\
+                cp $(m) $(strip $(2))/ -rfa;\
+                echo $(4)/$(notdir $(m)): >> $(3)))
+endef
+
+define ARM_NPU_DEP_INSTALL_TARGET_CMDS
+        $(call copy-arm-npu,$(@D),\
+                $(shell echo $(NPU_KO_INSTALL_DIR)),\
+                $(shell echo $(ARM_NPU_DEP)),\
+                $(ARM_NPU_MODULE_DIR))
+endef
+
 NPU_INSTALL_TARGETS_CMDS = \
 	$(INSTALL) -m 0755 $(@D)/build/sdk/drivers/galcore.ko $(NPU_KO_INSTALL_DIR); \
 	$(INSTALL) -m 0755 $(@D)/build/sdk/drivers/libGAL.so $(NPU_SO_INSTALL_DIR); \
@@ -30,6 +48,7 @@ endef
 define NPU_INSTALL_TARGET_CMDS
 	mkdir -p $(NPU_KO_INSTALL_DIR)
 	$(NPU_INSTALL_TARGETS_CMDS)
+	$(ARM_NPU_DEP_INSTALL_TARGET_CMDS)
 endef
 
 endif
