cc60d5ed5d24352f2b84638e428c08ba13e33cc9
Renjun Xu <renjun.xu@amlogic.com>
2018-10-31 13:48:53 +0800

    GVSDK: Update assistant sdk [1/1]
    PD#SWPL-1183
    
    Problem:
    1. modify sound card config.
    2. add install assistant config file.
    
    Solution:
    sound card and config is ok.
    
    Verify:
    Verified on S400/s420
    
    Change-Id: I74a02134f95936818d745ca46946f45baa6a89df
    Signed-off-by: Renjun Xu <renjun.xu@amlogic.com>

--- package/assistant-sdk/assistant-sdk.mk
+++ package/assistant-sdk/assistant-sdk.mk
@@ -11,6 +11,7 @@ ASSISTANT_SDK_SITE_METHOD = local
 define ASSISTANT_SDK_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/bin/* $(TARGET_DIR)/usr/bin/
     $(INSTALL) -D -m 0755 $(@D)/lib/* $(TARGET_DIR)/usr/lib/
+    $(INSTALL) -D -m 0644 $(@D)/config/* $(TARGET_DIR)/etc/
 endef
 
 $(eval $(generic-package))
