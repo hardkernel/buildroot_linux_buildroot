7c4241e1567ccd63513dd9fb92435e4593a99c08
Dianzhong Huo <dianzhong.huo@amlogic.com>
2018-11-22 19:08:07 +0800

    buildroot: Add opencv 3.4.3 support in w400/g12b_skt [1/1]
    PD#SH-139
    
    Problem:
    Opencv3 was not eable in buildroot
    
    Solution:
    Enable opencv3 config in w400/g12b_skt
    
    Verify:
    Local on w400/w411
    
    Change-Id: Icd3700470a5c5a1cc185ee2ea5dbf74644a9f981
    Signed-off-by: Dianzhong Huo <dianzhong.huo@amlogic.com>

--- package/opencv3/opencv3.mk
+++ package/opencv3/opencv3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCV3_VERSION = 3.1.0
+OPENCV3_VERSION = 3.4.3
 OPENCV3_SITE = $(call github,itseez,opencv,$(OPENCV3_VERSION))
 OPENCV3_INSTALL_STAGING = YES
 OPENCV3_LICENSE = BSD-3c
