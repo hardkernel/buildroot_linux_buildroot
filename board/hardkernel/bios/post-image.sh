85e2f5a4e60c8226bef7ba7fe42bb28870d5f8d4
Dongjin Kim <tobetter@gmail.com>
2018-12-05 15:06:13 +0900

    ODROID-N2: bios: generating spi-update.img to flash
    Change-Id: I41384b6b810f7373f077291eb589df5dd811635a
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

--- board/hardkernel/bios/post-image.sh
+++ board/hardkernel/bios/post-image.sh
@@ -20,20 +20,6 @@ ${HOST_DIR}/usr/bin/mkimage -A arm64 -T kernel -O linux -C gzip \
 	-d ${BINARIES_DIR}/Image.gz \
 	${BINARIES_DIR}/uImage
 
-${HOST_DIR}/usr/bin/mkimage -A arm64 -T script -O linux -C none \
-	-d ${BINARIES_DIR}/boot.cmd ${BINARIES_DIR}/boot.scr
-
-genimage --config "${GENIMAGE_CFG}"	\
-	--rootpath "${TARGET_DIR}"	\
-	--tmppath "${GENIMAGE_TMP}"	\
-	--inputpath "${BINARIES_DIR}"	\
-	--outputpath "${BINARIES_DIR}"
-
-#
-# Generating MMC boot image
-#
-dd if=${BINARIES_DIR}/u-boot.bin of=${BINARIES_DIR}/sdcard.img bs=512 seek=1 conv=fsync,notrunc
-
 #
 # Generating SPI boot image
 #
@@ -45,3 +31,44 @@ dd if=${BINARIES_DIR}/u-boot.bin of=${SPIBOOT_IMAGE} bs=512 seek=0 conv=fsync,no
 dd if=${BINARIES_DIR}/s922d_odroidn2.dtb of=${SPIBOOT_IMAGE} bs=512 seek=2048 conv=fsync,notrunc
 dd if=${BINARIES_DIR}/uImage of=${SPIBOOT_IMAGE} bs=512 seek=2248 conv=fsync,notrunc
 dd if=${BINARIES_DIR}/rootfs.cpio.uboot of=${SPIBOOT_IMAGE} bs=512 seek=10038 conv=fsync,notrunc
+
+cat>${BINARIES_DIR}/spiupdate.cmd<<__EOF
+load mmc 0 \${loadaddr} spiboot.img
+sf probe
+sf erase 0 0x800000
+sf write \${loadaddr} 0 \${filesize}
+echo "..#######..##....##"
+echo ".##.....##.##...##."
+echo ".##.....##.##..##.."
+echo ".##.....##.#####..."
+echo ".##.....##.##..##.."
+echo ".##.....##.##...##."
+echo "..#######..##....##"
+echo
+echo "SPI memory is updated successfully."
+echo "Please remove SDCARD and slide the boot switch to 'SPI'"
+echo "Then, reboot!!"
+sleep 60
+reboot
+__EOF
+
+${HOST_DIR}/usr/bin/mkimage -A arm64 -T script -O linux -C none \
+	-d ${BINARIES_DIR}/spiupdate.cmd ${BINARIES_DIR}/boot.scr
+
+#
+# Dropping MMC boot image
+#
+genimage --config "${GENIMAGE_CFG}"	\
+	--rootpath "${TARGET_DIR}"	\
+	--tmppath "${GENIMAGE_TMP}"	\
+	--inputpath "${BINARIES_DIR}"	\
+	--outputpath "${BINARIES_DIR}"
+#
+# Generating MMC boot image
+#
+dd if=${BINARIES_DIR}/u-boot.bin of=${BINARIES_DIR}/sdcard.img bs=512 seek=1 conv=fsync,notrunc
+
+#
+# Generating SPI update image
+#
+dd if=${BINARIES_DIR}/u-boot.bin of=${BINARIES_DIR}/spi-update.img bs=512 seek=1 conv=fsync,notrunc
