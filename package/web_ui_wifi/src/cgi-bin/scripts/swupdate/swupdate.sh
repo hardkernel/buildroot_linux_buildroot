7c3751b9dd7ca334675a38b8403a1e0fde99641f
Zhigang <zhigang.yu@amlogic.com>
2019-01-04 15:35:34 +0800

    ota: add web ui swupdate support for emmc [1/1]
    PD#SH-248
    
    Problem:
    on emmc flash can not run swupdate for web ui
    
    Solution:
    swupdate.sh add check /proc/inand for nand and emmc
    
    Verify:
    verify by w411
    
    Change-Id: I28af19df9abf29ed5f83680da48eb2aafeee8885
    Signed-off-by: Zhigang <zhigang.yu@amlogic.com>

--- package/web_ui_wifi/src/cgi-bin/scripts/swupdate/swupdate.sh
+++ package/web_ui_wifi/src/cgi-bin/scripts/swupdate/swupdate.sh
@@ -2,7 +2,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 if [ $1 == "start" ];then
-swupdate -r -l 6 -k /etc/swupdate-public.pem -b "0 1 2 3 4" -w "-document_root /var/www/swupdate/" > /tmp/swupdate.log &
+  if [ -f "/proc/inand" ]; then
+    swupdate -r -l 6 -k /etc/swupdate-public.pem -w "-document_root /var/www/swupdate/" > /tmp/swupdate.log &
+  else
+    swupdate -r -l 6 -k /etc/swupdate-public.pem -b "0 1 2 3 4" -w "-document_root /var/www/swupdate/" > /tmp/swupdate.log &
+  fi
 else
 killall swupdate
 fi
