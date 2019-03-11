fceedbc9932ff37da37676c13b9dd259312c0790
Jun.Zhang <jun.zhang@amlogic.com>
2019-03-11 09:17:31 +0800

    IPC: clear onvif_rtsp folder [1/1]
    PD#SWPL-1596
    
    Problem:
     onvif_rtsp need clean
    
    Solution:
     clean the unneeded files
    
    Verify:
    local
    
    Change-Id: I8d1b63434b65c58fa0ae35f3e938185772addb07
    Signed-off-by: Jun.Zhang <jun.zhang@amlogic.com>

--- package/ip-camera/onvif_rtsp/onvif_rtsp-0.1/onvif_rtsp_v4l2ctl.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ONVIF_RTSP_V4L2CTL_H_
-#define _ONVIF_RTSP_V4L2CTL_H_
-
-void onvif_rtsp_v4l2ctl_wdr (int fd, bool enable);
-void onvif_rtsp_v4l2ctl_fps (int fd, int fps);
-
-#endif /* _ONVIF_RTSP_V4L2CTL_H_ */
