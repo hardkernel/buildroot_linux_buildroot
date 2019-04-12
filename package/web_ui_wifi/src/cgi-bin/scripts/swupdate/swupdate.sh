034a04263392d26f710720ffc2f027083ac881c8
Blance Tang <guofeng.tang@amlogic.com>
2019-04-12 15:36:36 +0800

    buildroot: Fix swupdate broken issue [1/1]
    PD#SWPL-1751
    
    Problem:
     swupdate feature broken after upgrade to 2018.11 version
    
    Solution:
     The customized swupdate parameter is conflicted in version, so we
     correct it in swupdate.sh from "-r" to "--recovery"
     Recreate 0003/0004 patch for swupdate
     Add missing library: ./lib/libatomic.so.1 in ramfslist
     set proper EXT2 partition size to 2048M
    
    Verify:
      Local
    
    Change-Id: Ia647a2f77caaabbf4578d39071ec4aecc2600668
    Signed-off-by: Blance Tang <guofeng.tang@amlogic.com>

--- package/web_ui_wifi/src/cgi-bin/scripts/swupdate/swupdate.sh
+++ package/web_ui_wifi/src/cgi-bin/scripts/swupdate/swupdate.sh
@@ -3,9 +3,9 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 if [ $1 == "start" ];then
   if [ -f "/proc/inand" ]; then
-    swupdate -r -l 6 -k /etc/swupdate-public.pem -w "-document_root /var/www/swupdate/" > /tmp/swupdate.log &
+    swupdate --recovery -l 6 -k /etc/swupdate-public.pem -w "-r /var/www/swupdate/" > /tmp/swupdate.log &
   else
-    swupdate -r -l 6 -k /etc/swupdate-public.pem -b "0 1 2 3 4" -w "-document_root /var/www/swupdate/" > /tmp/swupdate.log &
+    swupdate --recovery -l 6 -k /etc/swupdate-public.pem -b "0 1 2 3 4" -w "-r /var/www/swupdate/" > /tmp/swupdate.log &
   fi
 else
 killall swupdate
