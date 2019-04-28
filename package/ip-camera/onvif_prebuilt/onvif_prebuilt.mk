e9447b2c529cdb23ebd5c2c25f44cf4643c0bcc6
Blance Tang <guofeng.tang@amlogic.com>
2019-04-28 08:38:32 +0800

    IPC: Put generated onvif prebuilt package to $(@D)/$(ONVIF_ARCH)  [1/1]
    PD#SWPL-6455
    
    Problem:
      Put genearted onvif prebuilt package to $(ONVIF_PREBUILT_SITE) will
      cause code fetch error in next build, because it introduce local
      change in that repository.
    
    Solution:
      Move it to $(@D)/$(ONVIF_ARCH)
    
    Verify:
      Local
    
    Change-Id: Iddc765286db827a01dd63490868d84831be140ab
    Signed-off-by: Blance Tang <guofeng.tang@amlogic.com>

--- package/ip-camera/onvif_prebuilt/onvif_prebuilt.mk
+++ package/ip-camera/onvif_prebuilt/onvif_prebuilt.mk
@@ -23,35 +23,34 @@ ifeq ($(BR2_PACKAGE_ONVIF_GENERATE_PREBUILT),y)
 #We will package the onvif prebuitl pacakge, so we need to wait for onvif libraries/binraries ready, here we list the onvif binraies.
 ONVIF_PREBUILT_DEPENDENCIES = onvif_srvd
 define ONVIF_PREBUILT_INSTALL_TARGET_CMDS
-	rm    -fr $(ONVIF_PREBUILT_DIRECTORY)
-	mkdir -p $(ONVIF_PREBUILT_DIRECTORY)
+	rm    -fr $(@D)/$(ONVIF_ARCH)/
+	mkdir -p $(@D)/$(ONVIF_ARCH)/
 
-	mkdir -p $(ONVIF_PREBUILT_DIRECTORY)/etc/init.d/
-	mkdir -p $(ONVIF_PREBUILT_DIRECTORY)/usr/bin
-	mkdir -p $(ONVIF_PREBUILT_DIRECTORY)/usr/lib/gstreamer-1.0/
+	mkdir -p $(@D)/$(ONVIF_ARCH)/etc/init.d/
+	mkdir -p $(@D)/$(ONVIF_ARCH)/usr/bin
+	mkdir -p $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
 
-	cp -a $(TARGET_DIR)/etc/ipc.json $(ONVIF_PREBUILT_DIRECTORY)/etc/
-	cp -a $(TARGET_DIR)/etc/onvif    $(ONVIF_PREBUILT_DIRECTORY)/etc/
-	cp -a $(TARGET_DIR)/etc/init.d/S80ipc-property-service $(ONVIF_PREBUILT_DIRECTORY)/etc/init.d/
-	cp -a $(TARGET_DIR)/etc/init.d/S91onvif_rtsp $(ONVIF_PREBUILT_DIRECTORY)/etc/init.d/
-	cp -a $(TARGET_DIR)/etc/init.d/S91onvif_srvd $(ONVIF_PREBUILT_DIRECTORY)/etc/init.d/
-	cp -a $(TARGET_DIR)/etc/init.d/S91onvif_wsdd $(ONVIF_PREBUILT_DIRECTORY)/etc/init.d/
+	cp -a $(TARGET_DIR)/etc/ipc.json                       $(@D)/$(ONVIF_ARCH)/etc/
+	cp -a $(TARGET_DIR)/etc/onvif                          $(@D)/$(ONVIF_ARCH)/etc/
+	cp -a $(TARGET_DIR)/etc/init.d/S80ipc-property-service $(@D)/$(ONVIF_ARCH)/etc/init.d/
+	cp -a $(TARGET_DIR)/etc/init.d/S91onvif_rtsp           $(@D)/$(ONVIF_ARCH)/etc/init.d/
+	cp -a $(TARGET_DIR)/etc/init.d/S91onvif_srvd           $(@D)/$(ONVIF_ARCH)/etc/init.d/
+	cp -a $(TARGET_DIR)/etc/init.d/S91onvif_wsdd           $(@D)/$(ONVIF_ARCH)/etc/init.d/
 
-	cp -a $(TARGET_DIR)/usr/bin/onvif_rtsp $(ONVIF_PREBUILT_DIRECTORY)/usr/bin/
-	cp -a $(TARGET_DIR)/usr/bin/onvif_srvd $(ONVIF_PREBUILT_DIRECTORY)/usr/bin/
-	cp -a $(TARGET_DIR)/usr/bin/onvif_wsdd $(ONVIF_PREBUILT_DIRECTORY)/usr/bin/
-	cp -a $(TARGET_DIR)/usr/bin/ipc-property $(ONVIF_PREBUILT_DIRECTORY)/usr/bin/
-	cp -a $(TARGET_DIR)/usr/bin/ipc-property-service $(ONVIF_PREBUILT_DIRECTORY)/usr/bin/
+	cp -a $(TARGET_DIR)/usr/bin/onvif_rtsp                 $(@D)/$(ONVIF_ARCH)/usr/bin/
+	cp -a $(TARGET_DIR)/usr/bin/onvif_srvd                 $(@D)/$(ONVIF_ARCH)/usr/bin/
+	cp -a $(TARGET_DIR)/usr/bin/onvif_wsdd                 $(@D)/$(ONVIF_ARCH)/usr/bin/
+	cp -a $(TARGET_DIR)/usr/bin/ipc-property               $(@D)/$(ONVIF_ARCH)/usr/bin/
+	cp -a $(TARGET_DIR)/usr/bin/ipc-property-service       $(@D)/$(ONVIF_ARCH)/usr/bin/
 
-	cp -a $(TARGET_DIR)/usr/lib/libipc-property.so $(ONVIF_PREBUILT_DIRECTORY)/usr/lib/
+	cp -a $(TARGET_DIR)/usr/lib/libipc-property.so                   $(@D)/$(ONVIF_ARCH)/usr/lib/
+	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlimgcap.so     $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
+	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlnn.so         $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
+	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamloverlay.so    $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
+	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlvenc.so       $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
+	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlfacenet.so    $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
 
-	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlimgcap.so $(ONVIF_PREBUILT_DIRECTORY)/usr/lib/gstreamer-1.0/
-	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlnn.so $(ONVIF_PREBUILT_DIRECTORY)/usr/lib/gstreamer-1.0/
-	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamloverlay.so $(ONVIF_PREBUILT_DIRECTORY)/usr/lib/gstreamer-1.0/
-	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlvenc.so $(ONVIF_PREBUILT_DIRECTORY)/usr/lib/gstreamer-1.0/
-	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlfacenet.so $(ONVIF_PREBUILT_DIRECTORY)/usr/lib/gstreamer-1.0/
-
-	tar -zcf $(TARGET_DIR)/../images/onvif-prebuilt-$(ONVIF_ARCH).tgz -C $(ONVIF_PREBUILT_SITE) $(ONVIF_ARCH)
+	tar -zcf $(TARGET_DIR)/../images/onvif-prebuilt-$(ONVIF_ARCH).tgz -C $(@D) $(ONVIF_ARCH)
 endef
 
 endif
