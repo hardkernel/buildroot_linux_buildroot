3f9949506f451082e5ef1d22f4504edb31f42a94
Daogao Xu <daogao.xu@amlogic.com>
2019-05-06 13:57:24 +0800

    AirPlay2: get source code and prebuilt from local [1/1]
    PD#SWPL-4821
    
    Problem:
    source code and prebuilt repository will be specified in manifests.xml
    
    Solution:
    get source code and prebuilt from local
    
    Verify:
    mesonaxg_s420_32_release
    
    Change-Id: I77d3a0fd037cd8874850d78c3c261fb028b96a0b
    Signed-off-by: Daogao Xu <daogao.xu@amlogic.com>

--- package/airplay2/airplay2.mk
+++ package/airplay2/airplay2.mk
@@ -11,17 +11,25 @@ ifeq ($(BR2_PACKAGE_GPTP),y)
 	AIRPLAY2_DEPENDENCIES += gptp
 	AIRPLAY2_MAKE_OPTS = sdkptp=0
 endif
-#AIRPLAY2_SITE_METHOD = local
-#AIRPLAY2_SITE = $(AIRPLAY2_PKGDIR)/airplayv2
+AIRPLAY2_SITE_METHOD = local
 #AIRPLAY2_ENABLE_DEBUG := y
 AIRPLAY2_BUILD_DIR := Release-linux
 ifeq ($(AIRPLAY2_ENABLE_DEBUG),y)
 	AIRPLAY2_MAKE_OPTS += debug=1
 	AIRPLAY2_BUILD_DIR := Debug-linux
 endif
-AIRPLAY2_SITE_METHOD = git
-AIRPLAY2_SITE = $(call qstrip,$(BR2_PACKAGE_AIRPLAY2_GIT_URL))
 AIRPLAY2_MAKE_OPTS += EXT_INCLUDES="-I$(STAGING_DIR)/usr/include/alsa -I$(STAGING_DIR)/usr/include/fdk-aac" EXT_LINKFLAGS="-ldns_sd -lasound -lfdk-aac"
+
+AIRPLAY2_LOCAL_SRC = $(wildcard $(TOPDIR)/../vendor/amlogic/airplayv2/airplayv2)
+AIRPLAY2_LOCAL_PREBUILT = $(wildcard $(TOPDIR)/../vendor/amlogic/airplayv2/airplayv2_prebuilt)
+AIRPLAY2_PREBUILT_DIRECTORY = $(call qstrip,$(BR2_ARCH)).$(call qstrip,$(BR2_GCC_TARGET_ABI)).$(call qstrip,$(BR2_GCC_TARGET_FLOAT_ABI))
+
+ifeq ($(or $(AIRPLAY2_LOCAL_SRC),$(AIRPLAY2_LOCAL_PREBUILT)),)
+$(error Neither AIRPLAY2_LOCAL_SRC nor AIRPLAY2_LOCAL_PREBUILT exists)
+endif
+
+ifneq ($(AIRPLAY2_LOCAL_SRC),)
+AIRPLAY2_SITE=$(AIRPLAY2_LOCAL_SRC)
 define AIRPLAY2_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) CROSSPREFIX=$(TARGET_CROSS) $(AIRPLAY2_MAKE_OPTS) -C $(@D)/AirPlaySDK/PlatformPOSIX
 	$(TARGET_MAKE_ENV) $(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) STRIP=$(TARGET_STRIP) debug=1 platform_makefile=Platform/Platform.include.mk -C $(@D)/WAC
@@ -29,12 +37,34 @@ endef
 
 define AIRPLAY2_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -D $(@D)/AirPlaySDK/build/$(AIRPLAY2_BUILD_DIR)/airplaydemo $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 755 -D $(AIRPLAY2_PKGDIR)S82airplay2 $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -m 755 -D $(@D)/WAC/WACServer $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 755 -D $(@D)/WAC/wac.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 755 -D $(AIRPLAY2_PKGDIR)S82airplay2 $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -m 755 -D $(AIRPLAY2_PKGDIR)wac.sh $(TARGET_DIR)/usr/bin/
 endef
 
-$(eval $(generic-package))
+ifneq ($(AIRPLAY2_LOCAL_PREBUILT),)
+$(info Both "$(AIRPLAY2_LOCAL_SRC)" and "$(AIRPLAY2_LOCAL_PREBUILT)" exist, will update prebuilt)
+define AIRPLAY2_UPDATE_PREBUILT_CMDS
 
+	# DO NOT remove the first empty line
+	$(INSTALL) -m 755 -D $(@D)/AirPlaySDK/build/$(AIRPLAY2_BUILD_DIR)/airplaydemo $(AIRPLAY2_LOCAL_PREBUILT)/$(AIRPLAY2_PREBUILT_DIRECTORY)/usr/bin/airplaydemo
+	$(INSTALL) -m 755 -D $(@D)/WAC/WACServer $(AIRPLAY2_LOCAL_PREBUILT)/$(AIRPLAY2_PREBUILT_DIRECTORY)/usr/bin/WACServer
+endef
+#	AIRPLAY2_INSTALL_TARGET_CMDS += $(AIRPLAY2_UPDATE_PREBUILT_CMDS)
+endif
+
+else # prebuilt
+
+AIRPLAY2_SITE=$(AIRPLAY2_LOCAL_PREBUILT)
+define AIRPLAY2_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -D $(@D)/$(AIRPLAY2_PREBUILT_DIRECTORY)/usr/bin/airplaydemo $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 755 -D $(@D)/$(AIRPLAY2_PREBUILT_DIRECTORY)/usr/bin/WACServer $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 755 -D $(AIRPLAY2_PKGDIR)S82airplay2 $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -m 755 -D $(AIRPLAY2_PKGDIR)wac.sh $(TARGET_DIR)/usr/bin/
+endef
+
+endif
+
+$(eval $(generic-package))
 
 include package/airplay2/gptp/gptp.mk
