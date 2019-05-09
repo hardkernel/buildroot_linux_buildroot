894920943d656d0544f1d768c44b5dc9c7f030c3
Blance Tang <guofeng.tang@amlogic.com>
2019-05-09 14:29:20 +0800

    buildroot: Only install used program to test_plan  [1/1]
    PD#SWPL-802
    
    Problem:
      Buildroot block us to install 32bit program into 64bit system
    
    Solution:
      Just install 32bit program into 32bit system
      Just install 64bit program into 64bit system
      Dont copy source code and prebuilt folder
    
    Verify:
      Local
    
    Change-Id: Ic3e34760c285e976ff7afd09db48e91fca84e56c
    Signed-off-by: Blance Tang <guofeng.tang@amlogic.com>

--- package/test_tools/test_tools.mk
+++ package/test_tools/test_tools.mk
@@ -8,7 +8,8 @@ TEST_TOOLS_SITE = $(TOPDIR)/../vendor/amlogic/test_tools
 TEST_TOOLS_SITE_METHOD = local
 
 define TEST_TOOLS_INSTALL_TARGET_CMDS
-	cp -rf  $(@D)/test_plan  ${TARGET_DIR}/
+	pushd $(@D)/test_plan; ./install_prebuilt_bin.sh $(BR2_ARCH); popd
+	rsync -arv  $(@D)/test_plan  ${TARGET_DIR}/ --exclude=prebuilt --exclude=src --exclude=install_prebuilt_bin.sh
 endef
 
 $(eval $(generic-package))
