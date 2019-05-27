35cf4e8d2c37b551b214493c1ad7d21bfbc3fe5c
Jun.Zhang <jun.zhang@amlogic.com>
2019-05-27 16:35:35 +0800

    IPC: add gdc plugin package [1/2]
    PD#SWPL-8901
    
    Problem:
     feature requiment
    
    Solution:
     - add gdc plugin package
    
    Verify:
    local
    
    Change-Id: I7d2d45d21b6b192a4c6b6baba5bfb26c70fe546f
    Signed-off-by: Jun.Zhang <jun.zhang@amlogic.com>

--- package/ip-camera/onvif_prebuilt/onvif_prebuilt.mk
+++ package/ip-camera/onvif_prebuilt/onvif_prebuilt.mk
@@ -49,6 +49,8 @@ define ONVIF_PREBUILT_INSTALL_TARGET_CMDS
 	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamloverlay.so    $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
 	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlvenc.so       $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
 	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlfacenet.so    $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
+	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlvconv.so    $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
+	cp -a $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstamlgdc.so    $(@D)/$(ONVIF_ARCH)/usr/lib/gstreamer-1.0/
 
 	tar -zcf $(TARGET_DIR)/../images/onvif-prebuilt-$(ONVIF_ARCH).tgz -C $(@D) $(ONVIF_ARCH)
 endef
