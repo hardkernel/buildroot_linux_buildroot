eda86a679454e6d7871295a47b6199a4b2963d8f
Jun.Zhang <jun.zhang@amlogic.com>
2019-06-03 15:10:47 +0800

    IPC: move out h264/h265 encoder library package [3/3]
    PD#SWPL-4163
    
    Problem:
     h264/h265 encoder libraries kept inside the ipc packages, not suitable
     for maintain
    
    Solution:
     - move out h264 and h265 packages
    
    Verify:
    local
    
    Change-Id: I3419127d72d46a44e9f8c84a18407f81780896bc
    Signed-off-by: Jun.Zhang <jun.zhang@amlogic.com>

--- package/ip-camera/libvpcodec/libvpcodec-1.0/enc/gx_enc_fast/parser.h
+++
@@ -1,5 +0,0 @@
-#ifndef _DUMP_H_
-#define _DUMP_H_
-
-extern int Parser_DumpInfo(gx_fast_enc_drv_t* p);
-#endif
---
+++ package/libvpcodec/libvpcodec.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# libvpcodec
+#
+################################################################################
+
+LIBVPCODEC_VERSION = 1.0
+LIBVPCODEC_SITE = $(TOPDIR)/../hardware/aml-4.9/amlogic/libencoder/h264
+LIBVPCODEC_SITE_METHOD = local
+LIBVPCODEC_DEPENDENCIES =
+LIBVPCODEC_LICENSE = LGPL
+LIBVPCODEC_INSTALL_STAGING = YES
+
+LIBVPCODEC_BUILD_DIR = bjunion_enc
+# This package uses the AML_LIBS_STAGING_DIR variable to construct the
+# header and library paths used when compiling
+define LIBVPCODEC_BUILD_CMDS
+	$(TARGET_MAKE_ENV) CC=$(TARGET_CC) CXX=$(TARGET_CXX)\
+	$(MAKE)	-C $(@D)/$(LIBVPCODEC_BUILD_DIR)
+	# build test app
+	$(TARGET_MAKE_ENV) CC=$(TARGET_CC) CXX=$(TARGET_CXX)\
+	$(MAKE)	-C $(@D)/$(LIBVPCODEC_BUILD_DIR)/../
+endef
+
+define LIBVPCODEC_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/$(LIBVPCODEC_BUILD_DIR)/vpcodec_1_0.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0755 $(@D)/$(LIBVPCODEC_BUILD_DIR)/libvpcodec.so $(STAGING_DIR)/usr/lib/
+endef
+
+define LIBVPCODEC_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/$(LIBVPCODEC_BUILD_DIR)/libvpcodec.so $(TARGET_DIR)/usr/lib/
+endef
+
+$(eval $(generic-package))
