951c93281e05e86c94a3beadb068ae98c6cc8345
Blance Tang <guofeng.tang@amlogic.com>
2019-06-10 15:06:24 +0800

    buildroot: Move audioservice/system_control/platinum Config.in [1/1]
    PD#SWPL-802
    
    Problem:
      audioservice/system_control/platinum's Config.in is not in
      buildroot/package, we can't configure these 3 modules as optional
    
    Solution:
      Move Config.in & xx.mk from these 3 modules to buildroot/package/xxx
    
    Verify:
      Local
    
    Change-Id: I4f5c01ef40110e3c299c9b16461a4ccfe888f26a
    Signed-off-by: Blance Tang <guofeng.tang@amlogic.com>
    (cherry picked from commit 482966b1ad5c94fb7c223e21aff718a09f356bfe)

---
+++ package/audioservice/audioservice.mk
@@ -0,0 +1,84 @@
+################################################################################
+#
+# audioservice
+#
+################################################################################
+
+AUDIOSERVICE_VERSION = 1.0.0
+AUDIOSERVICE_SITE = $(TOPDIR)/../vendor/amlogic/audioservice
+AUDIOSERVICE_SITE_METHOD = local
+#AUDIOSERVICE_LICENSE = GPLv2+, GPLv2 (py-smbus)
+#AUDIOSERVICE_LICENSE_FILES = COPYING
+AUDIOSERVICE_INSTALL_STAGING = YES
+AUDIOSERVICE_AUTORECONF = YES
+
+AUDIOSERVICE_DEPENDENCIES = libglib2 dbus cjson aml_halaudio
+
+AUDIOSERVICE_CONF_OPTS += --enable-halaudio
+
+ifeq ($(BR2_PACKAGE_AUDIOSERVICE_ASR),y)
+AUDIOSERVICE_CONF_OPTS += --enable-asr
+endif
+
+ifeq ($(BR2_PACKAGE_AUDIOSERVICE_PULSEAUDIO),y)
+AUDIOSERVICE_CONF_OPTS += --enable-pulseaudio
+AUDIOSERVICE_DEPENDENCIES += pulseaudio
+endif
+
+ifeq ($(BR2_PACKAGE_AUDIOSERVICE_FFMPEG),y)
+AUDIOSERVICE_CONF_OPTS += --enable-ffmpeg
+AUDIOSERVICE_DEPENDENCIES += ffmpeg
+endif
+
+ifeq ($(BR2_PACKAGE_AUDIOSERVICE_PYTHON),y)
+AUDIOSERVICE_CONF_OPTS += --enable-python
+AUDIOSERVICE_DEPENDENCIES += python
+endif
+
+ifeq ($(BR2_PACKAGE_AUDIOSERVICE_EXTERNAL),y)
+AUDIOSERVICE_CONF_OPTS += --enable-external
+endif
+
+ifeq ($(BR2_PACKAGE_AUDIOSERVICE_S400_SBR),y)
+AUDIOSERVICE_CONF_OPTS += --enable-s400sbr
+endif
+
+ifeq ($(BR2_PACKAGE_AUDIOSERVICE_USBPLAYER),y)
+AUDIOSERVICE_CONF_OPTS += --enable-usbplayer
+AUDIOSERVICE_DEPENDENCIES += ffmpeg aml_audio_player
+endif
+
+ifeq ($(BR2_PACKAGE_AUDIOSERVICE_AIRPLAY),y)
+AUDIOSERVICE_CONF_OPTS += --enable-airplay
+AUDIOSERVICE_DEPENDENCIES += airplay2
+endif
+
+
+define AUDIOSERVICE_LIB_INSTALL_CMD
+
+endef
+
+
+AUDIOSERVICe_CONF_OPTS = --prefix=$(TARGET_DIR)/usr
+
+define AUDIOSERVICE_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 $(AUDIOSERVICE_SITE)/script/S90audioservice \
+		$(TARGET_DIR)/etc/init.d/S90audioservice
+	$(INSTALL) -D -m 644 \
+		$(AUDIOSERVICE_SITE)/src/config/$(BR2_PACKAGE_AUDIOSERVICE_CONFIG_FILE) \
+		$(TARGET_DIR)/etc/default_audioservice.conf
+	$(INSTALL) -d $(TARGET_DIR)/etc/mcu6350_bin/
+	$(INSTALL) -D -m 644 \
+		$(AUDIOSERVICE_SITE)/src/external/mcu6350_bin/* $(TARGET_DIR)/etc/mcu6350_bin/
+endef
+
+# Autoreconf requires an m4 directory to exist
+define AUDIOSERVICE_PATCH_M4
+	mkdir -p $(@D)/m4
+endef
+
+AUDIOSERVICE_POST_PATCH_HOOKS += AUDIOSERVICE_PATCH_M4
+
+
+
+$(eval $(autotools-package))
