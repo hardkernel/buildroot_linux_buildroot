37fd02a465c45af9378edbf1227717c19bf0e409
lihengrui <Hengrui.Li@amlogic.com>
2019-06-14 19:16:06 +0800

    buildroot: add ipc-webui package  [1/1]
    PD#SWPL-8235
    
    Problem:
       web tool
    
    Solution:
        create package
    
    Verify:
    local
    
    Change-Id: Icdae44842da02cae88e3fa17d7b005910caa2d74
    Signed-off-by: lihengrui <Hengrui.Li@amlogic.com>

---
+++ package/ip-camera/ipc-webui/ipc-webui.mk
@@ -0,0 +1,27 @@
+#############################################################
+#
+# IPC Webui
+#
+#############################################################
+
+IPC_WEBUI_VERSION = 1.0
+IPC_WEBUI_SITE = $(TOPDIR)/../vendor/amlogic/ipc/ipc_webui
+IPC_WEBUI_SITE_METHOD = local
+IPC_WEBUI_DEPENDENCIES = nginx php
+define IPC_WEBUI_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/var/www/ipc-webui
+
+	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/index.php $(TARGET_DIR)/var/www/ipc-webui
+	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/check_database.php $(TARGET_DIR)/var/www/ipc-webui
+	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/common.php $(TARGET_DIR)/var/www/ipc-webui
+	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/sqlite_create.sql $(TARGET_DIR)/var/www/ipc-webui
+	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/default.config/nginx.conf $(TARGET_DIR)/etc/nginx/nginx.conf
+	$(INSTALL) -D -m 755 $(IPC_WEBUI_PKGDIR)/S49ipc_webui $(TARGET_DIR)/etc/init.d
+
+	cp -af $(IPC_WEBUI_SITE)/facetable $(TARGET_DIR)/var/www/ipc-webui
+	cp -af $(IPC_WEBUI_SITE)/usertable $(TARGET_DIR)/var/www/ipc-webui
+	cp -af $(IPC_WEBUI_SITE)/layui $(TARGET_DIR)/var/www/ipc-webui
+endef
+
+$(eval $(generic-package))
+
