0d26ed0f3e9bfc4219f4c822d7057576b4b617ae
lihengrui <hengrui.li@amlogic.com>
2019-06-24 10:05:52 +0800

    buildroot: add uploading function [1/1]
    PD#SWPL-9586
    
    Problem:
        - add customized database uploading function
    
    Solution:
        - modify Config.in
        - modify ipc-webui.mk
        - modify onvif_prebuilt.mk
    
    Verify:
    local
    
    Change-Id: I7b8bf7615c042c2b0cd145a9a38b8bc56e72c3c1
    Signed-off-by: lihengrui <hengrui.li@amlogic.com>

--- package/ip-camera/ipc-webui/ipc-webui.mk
+++ package/ip-camera/ipc-webui/ipc-webui.mk
@@ -16,10 +16,13 @@ define IPC_WEBUI_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/common.php $(TARGET_DIR)/var/www/ipc-webui
 	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/sqlite_create.sql $(TARGET_DIR)/var/www/ipc-webui
 	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/default.config/nginx.conf $(TARGET_DIR)/etc/nginx/nginx.conf
+	$(INSTALL) -D -m 755 $(IPC_WEBUI_SITE)/default.config/php.ini $(TARGET_DIR)/etc/php.ini
 	$(INSTALL) -D -m 755 $(IPC_WEBUI_PKGDIR)/S49ipc_webui $(TARGET_DIR)/etc/init.d
+
 
 	cp -af $(IPC_WEBUI_SITE)/facetable $(TARGET_DIR)/var/www/ipc-webui
 	cp -af $(IPC_WEBUI_SITE)/usertable $(TARGET_DIR)/var/www/ipc-webui
+	cp -af $(IPC_WEBUI_SITE)/upload    $(TARGET_DIR)/var/www/ipc-webui
 	cp -af $(IPC_WEBUI_SITE)/layui $(TARGET_DIR)/var/www/ipc-webui
 endef
 
