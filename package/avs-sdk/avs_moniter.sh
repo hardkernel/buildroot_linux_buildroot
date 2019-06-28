909afc4be1a82fce8fb4c8f4a6c1d6cfb7ff93a7
Renjun Xu <renjun.xu@amlogic.com>
2019-06-28 14:47:19 +0800

    AVS: Add AVS configuration and modify avs_moniter [1/1]
    PD#SWPL-10434
    
    Problem:
    AVS can't wake up for A113L A1 platform
    
    Solution:
    Add AVS config and modify avs mic channel
    
    Verify:
    Local
    
    Change-Id: I2c4c0f6c8353b2399d588dfcf924fca43594ef12
    Signed-off-by: Renjun Xu <renjun.xu@amlogic.com>

--- package/avs-sdk/avs_moniter.sh
+++ package/avs-sdk/avs_moniter.sh
@@ -4,11 +4,26 @@ CONFIG=/etc/AlexaClientSDKConfig.json
 PROCESS=/usr/bin/SampleApp
 DISPLAYCARD=/sbin/DisplayCardsD
 
+if cat /proc/device-tree/compatible | grep "amlogic,a1"; then
+    A113L_PLF=1
+fi
+
 start_avs() {
+    local channel=0
     if [ -f $CONFIG ];then
         mkdir -p /data/share/avs/
         cd /usr/bin/
-        ./SampleApp $CONFIG NONE back &
+        if [ -n "$A113L_PLF" ]
+        then
+            echo "get amlogic A1 board"
+            /usr/bin/switch_mics_num.sh 2
+            let channel=4
+        else
+            echo "get amlogic A113 board"
+            /usr/bin/switch_mics_num.sh 6
+            let channel=16
+        fi
+        ./SampleApp $CONFIG NONE back $channel &
         cd -
     else
         echo "avs start fail: not found config file $CONFIG!!!"
@@ -19,20 +34,22 @@ start_speaker_process () {
     /usr/bin/speaker_process &
 }
 
-modprobe snd-aloop
+if [ -z "$A113L_PLF" ]; then
+    modprobe snd-aloop
 
-amixer_ctr=`amixer controls | grep "Loopback Enable"` > /dev/null
-loop_id=${amixer_ctr%%,*}
-amixer cset $loop_id 1
+    amixer_ctr=`amixer controls | grep "Loopback Enable"` > /dev/null
+    loop_id=${amixer_ctr%%,*}
+    amixer cset $loop_id 1
 
-amixer_ctr=`amixer controls | grep "datain_datalb_total"` > /dev/null
-datalb_id=${amixer_ctr%%,*}
-amixer cset $datalb_id 16
+    amixer_ctr=`amixer controls | grep "datain_datalb_total"` > /dev/null
+    datalb_id=${amixer_ctr%%,*}
+    amixer cset $datalb_id 16
 
-if [ -f $DISPLAYCARD ]; then
-    $DISPLAYCARD -r 270 &
-else
-    echo "displaycard start fail!"
+    if [ -f $DISPLAYCARD ]; then
+        $DISPLAYCARD -r 270 &
+    else
+        echo "displaycard start fail!"
+    fi
 fi
 
 if [ -r /etc/last_date ]; then
@@ -44,10 +61,14 @@ if [ -r /etc/last_date ]; then
 fi
 
 while true ; do
-    ps -fe|grep speaker_process |grep -v grep 1>/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo "start speaker_process....."
-        start_speaker_process
+    if [ -z "$A113L_PLF" ]; then
+        ps -fe|grep speaker_process |grep -v grep 1>/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "start speaker_process....."
+            start_speaker_process
+        fi
+        #Just avoid script error incase SBR will delete speaker process
+        echo "" > /dev/null
     fi
 
     ps -fe|grep SampleApp |grep -v grep 1>/dev/null 2>&1
