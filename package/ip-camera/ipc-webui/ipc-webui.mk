86fcce00e9ed2db25dd926d7551da932a04d40dc
lihengrui <hengrui.li@amlogic.com>
2019-07-09 15:06:05 +0800

    buildroot: save database via webui tool [1/1]
    PD#SWPL-10968
    
    Problem:
        save database to localhost
    
    Solution:
        add a new file - save_database.php
    
    Verify:
    local
    
    Change-Id: I093bdbf46df3aa3a3f66324e40c13d5118fc43cc
    Signed-off-by: lihengrui <hengrui.li@amlogic.com>

--- package/ip-camera/ipc-webui/ipc-webui.mk
+++ package/ip-camera/ipc-webui/ipc-webui.mk
@@ -11,21 +11,12 @@ IPC_WEBUI_DEPENDENCIES = nginx php nodejs
 define IPC_WEBUI_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/var/www/ipc-webui
 
-	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/index.php $(TARGET_DIR)/var/www/ipc-webui
-	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/check_database.php $(TARGET_DIR)/var/www/ipc-webui
-	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/common.php $(TARGET_DIR)/var/www/ipc-webui
-	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/view-stream.php $(TARGET_DIR)/var/www/ipc-webui
-	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/sqlite_create.sql $(TARGET_DIR)/var/www/ipc-webui
 	$(INSTALL) -D -m 644 $(IPC_WEBUI_SITE)/default.config/nginx.conf $(TARGET_DIR)/etc/nginx/nginx.conf
 	$(INSTALL) -D -m 755 $(IPC_WEBUI_SITE)/default.config/php.ini $(TARGET_DIR)/etc/php.ini
 	$(INSTALL) -D -m 755 $(IPC_WEBUI_PKGDIR)/S49ipc_webui $(TARGET_DIR)/etc/init.d
 
+	rsync -avz $(IPC_WEBUI_SITE)/ $(TARGET_DIR)/var/www/ipc-webui --exclude 'default.config' --exclude '.git'
 
-	cp -af $(IPC_WEBUI_SITE)/facetable $(TARGET_DIR)/var/www/ipc-webui
-	cp -af $(IPC_WEBUI_SITE)/usertable $(TARGET_DIR)/var/www/ipc-webui
-	cp -af $(IPC_WEBUI_SITE)/upload    $(TARGET_DIR)/var/www/ipc-webui
-	cp -af $(IPC_WEBUI_SITE)/layui $(TARGET_DIR)/var/www/ipc-webui
-	cp -af $(IPC_WEBUI_SITE)/jsmpeg $(TARGET_DIR)/var/www/ipc-webui
 endef
 
 $(eval $(generic-package))
