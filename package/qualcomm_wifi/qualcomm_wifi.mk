01a4de7e1b1d376ddf4305f242c8f235cf40aa2f
jiabin.chen <jiabin.chen@amlogic.com>
2019-11-04 10:15:54 +0800

    wifi: add SM1-AC214 WiFi Bringup on kernel4.9 [1/2]
    PD#SWPL-15577
    
    Problem:
    Buildroot SM1-AC214 WiFi Bringup
    
    Solution:
    modified qualcomm_wifi.mk
    
    Verify:
    SM1-AC214
    
    Change-Id: I42df97e2cb12bb0f706cf225a7b5b578e2a1fbc5
    Signed-off-by: jiabin.chen <jiabin.chen@amlogic.com>

--- package/qualcomm_wifi/qualcomm_wifi.mk
+++ package/qualcomm_wifi/qualcomm_wifi.mk
@@ -16,8 +16,6 @@ QUALCOMM_MODULE_INSTALL_DIR = $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/
 
 QUALCOMM_WIFI_DEPENDENCIES = linux
 
-CFLAGS += -DCONFIG_BUILDROOT
-
 ifeq ($(BR2_PACKAGE_QUALCOMM_QCA9377),y)
 define QUALCOMM_QCA9377_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE)  -C $(@D)/qca9377/AIO/build ARCH=$(KERNEL_ARCH) \
@@ -29,18 +27,18 @@ define QUALCOMM_QCA9377_INSTALL_CMDS
 	$(INSTALL) -m 0666 $(@D)/qca9377/AIO/rootfs-x86-android.build/lib/modules/wlan.ko $(QUALCOMM_MODULE_INSTALL_DIR)
 	echo $(QUALCOMM_MODULE_DIR)/wlan.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.de
 	mkdir -p $(TARGET_DIR)/lib/firmware/qca
-	mkdir -p $(TARGET_DIR)/lib/firmware/qca6174
-	mkdir -p $(TARGET_DIR)/lib/firmware/qca6174/wlan
-	$(INSTALL) -D -m 0644 $(WIFI_FW_SITE)/qcom/config/qca6174/wifi/*.bin $(TARGET_DIR)/lib/firmware/qca6174/
-	$(INSTALL) -D -m 0644 $(WIFI_FW_SITE)/qcom/config/qca6174/wifi/wlan/* $(TARGET_DIR)/lib/firmware/qca6174/wlan/
-	$(INSTALL) -D -m 0644 $(WIFI_FW_SITE)/qcom/config/qca6174/bt/* $(TARGET_DIR)/lib/firmware/qca/
+	mkdir -p $(TARGET_DIR)/lib/firmware/qca9377
+	mkdir -p $(TARGET_DIR)/lib/firmware/qca9377/wlan
+	$(INSTALL) -D -m 0644 $(WIFI_FW_SITE)/qcom/config/qca9377/wifi_49/*.bin $(TARGET_DIR)/lib/firmware/qca9377/
+	$(INSTALL) -D -m 0644 $(WIFI_FW_SITE)/qcom/config/qca9377/wifi_49/wlan/* $(TARGET_DIR)/lib/firmware/qca9377/wlan/
+	$(INSTALL) -D -m 0644 $(WIFI_FW_SITE)/qcom/config/qca9377/bt_bluez/* $(TARGET_DIR)/lib/firmware/qca/
 endef
 endif
 
 ifeq ($(BR2_PACKAGE_QUALCOMM_QCA6174),y)
 define QUALCOMM_QCA6174_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE)  -C $(@D)/qca6174/AIO/build ARCH=$(KERNEL_ARCH) \
-		KERNELPATH=$(LINUX_DIR)	CROSS_COMPILE=$(TARGET_KERNEL_CROSS) CONFIG_BUILDROOT=y
+		KERNELPATH=$(LINUX_DIR)	CROSS_COMPILE=$(TARGET_KERNEL_CROSS)  CONFIG_BUILDROOT=y
 endef
 
 define QUALCOMM_QCA6174_INSTALL_CMDS
