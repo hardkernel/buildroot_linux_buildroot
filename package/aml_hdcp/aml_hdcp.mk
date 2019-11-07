f5b96fa7edb2bb4a7d38d1246c3fc4db0538a283
Blance Tang <guofeng.tang@amlogic.com>
2019-11-07 18:01:57 +0800

    buildroot: Move AML components under aml_ folder [1/1]
    PD#SWPL-802
    
    Problem:
      Move AML components under aml_ folder
    
    Solution:
      Move AML components under aml_ folder
    
    Verify:
      Local
    
    Change-Id: I8523ff9f7b98c9100d76f6cbc297d2906723556b
    Signed-off-by: Blance Tang <guofeng.tang@amlogic.com>

--- package/aml_hdcp/aml_hdcp.mk
+++ package/aml_hdcp/aml_hdcp.mk
@@ -3,20 +3,20 @@
 # HDCP driver
 #
 #############################################################
-HDCP_SITE = $(TOPDIR)/../vendor/amlogic/hdcp
-HDCP_SITE_METHOD = local
+AML_HDCP_SITE = $(TOPDIR)/../vendor/amlogic/hdcp
+AML_HDCP_SITE_METHOD = local
 
 
 
-define HDCP_BUILD_CMDS
-	@echo "hdcp comiple"
+define AML_HDCP_BUILD_CMDS
+	@echo "aml hdcp comiple"
 endef
 
-define HDCP_INSTALL_TARGET_CMDS
+define AML_HDCP_INSTALL_TARGET_CMDS
 	-mkdir -p $(TARGET_DIR)/lib/teetz/
 	$(INSTALL) -D -m 0755 $(@D)/ca/bin/tee_hdcp $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 0755 $(@D)/ta/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta $(TARGET_DIR)/lib/teetz/
-	install -m 755 package/hdcp/S60hdcp $(TARGET_DIR)/etc/init.d/S60hdcp
+	install -m 755 package/aml_hdcp/S60hdcp $(TARGET_DIR)/etc/init.d/S60hdcp
 endef
 
 $(eval $(generic-package))
--- package/aml_provision/aml_provision.mk
+++ package/aml_provision/aml_provision.mk
@@ -1,18 +1,18 @@
 #############################################################
 #
-# PROVISION driver
+# AML PROVISION driver
 #
 #############################################################
-PROVISION_SITE = $(TOPDIR)/../vendor/amlogic/provision
-PROVISION_SITE_METHOD = local
+AML_PROVISION_SITE = $(TOPDIR)/../vendor/amlogic/provision
+AML_PROVISION_SITE_METHOD = local
 
 
 
-define PROVISION_BUILD_CMDS
-	@echo "provision comiple"
+define AML_PROVISION_BUILD_CMDS
+	@echo "aml provision comiple"
 endef
 
-define PROVISION_INSTALL_TARGET_CMDS
+define AML_PROVISION_INSTALL_TARGET_CMDS
 	-mkdir -p $(TARGET_DIR)/lib/teetz/
 	$(INSTALL) -D -m 0755 $(@D)/ca/bin/tee_provision $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 0755 $(@D)/ta/e92a43ab-b4c8-4450-aa12b1516259613b.ta $(TARGET_DIR)/lib/teetz/
