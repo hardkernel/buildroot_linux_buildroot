e41fcaa0ede746071e43860bbc819e55e5937db4
Blance Tang <guofeng.tang@amlogic.com>
2019-11-08 19:39:27 +0800

    buildroot: Merge neucore team change [1/1]
    PD#SWPL-802
    
    Problem:
      Merge neucore team change
    
    Solution:
      Merge neucore team change
    
    Verify:
      Local
    
    Change-Id: If94c335fb845b9f5382cb39cf1a2685c50caf2ef
    Signed-off-by: Blance Tang <guofeng.tang@amlogic.com>

--- board/neucore/mesonc1_ae401/rootfs/etc/property_set.sh
+++ board/neucore/mesonc1_ae401/rootfs/etc/property_set.sh
@@ -12,3 +12,30 @@ export media_libplayer_modules=libcurl_mod.so,libdash_mod.so,libvhls_mod.so
 #Graphic enviroment
 #wayland
 export XDG_RUNTIME_DIR=/run/wayland/
+export LD_LIBRARY_PATH=/home/neucore/lib/:/home/neucore/lib/gstreamer-1.0:$LD_LIBRARY_PATH
+export PATH=/home/neucore/bin/:$PATH
+export GSETTINGS_SCHEMA_DIR=/home/neucore/conf
+if [ ! -d "/tmp/dbus" ]; then
+   mkdir /tmp/dbus
+fi
+
+HOME=/root
+export DBUS_SESSION_BUS_ADDRESS=unix:abstract=/tmp/dbus-123456
+touch /tmp/apprun
+source /tmp/apprun
+if [ $x == 1 ]; then
+  pidof dbus-daemon 2&>1 > /dev/null
+  if [ $? -ne 0 ]; then	
+    dbus-daemon --session --print-address --fork --address=unix:abstract=/tmp/dbus-123456
+    echo "export DBUS_SESSION_BUS_ADDRESS=unix:abstract=/tmp/dbus-123456" > /tmp/dbus/dbus-addr
+    echo "DBUS_SESSION_BUS_ADDRESS='unix:abstract=/tmp/dbus-123456'" > /tmp/dbus/dbus.env
+    echo "export HOME=/root" >> /tmp/dbus/dbus-addr
+    start-stop-daemon -b -m -S -q -p "/var/run/start.pid" -x /home/neucore/script/sbs_init.sh
+  fi
+fi
+
+echo "x=1" > /tmp/apprun
+echo "export x" >> /tmp/apprun
+source /tmp/apprun
+source /tmp/dbus/dbus-addr
+rm /1 /root/1 -rf
