1c42949556cf840392d70a2eb4c608e1fe8dc670
Dongjin Kim <tobetter@gmail.com>
2020-03-04 19:38:23 +0900

    ODROID-COMMON: package/kexec: upgrade 'kexec' to v2.0.18
    Change-Id: I289ebb4c03f201ad5ca2292c6b29460ebc9f20df
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

--- package/kexec/kexec.mk
+++ package/kexec/kexec.mk
@@ -4,11 +4,14 @@
 #
 ################################################################################
 
-KEXEC_VERSION = 2.0.18
-KEXEC_SOURCE = kexec-tools-$(KEXEC_VERSION).tar.xz
-KEXEC_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kexec
-KEXEC_LICENSE = GPL-2.0
+KEXEC_VERSION = v2.0.18
+KEXEC_SITE = git://git.kernel.org/pub/scm/utils/kernel/kexec/kexec-tools.git
+KEXEC_SITE_METHOD = git
+KEXEC_INSTALL_STAGING = YES
+KEXEC_LICENSE = GPLv2
 KEXEC_LICENSE_FILES = COPYING
+
+HOST_AUTOMAKE_DEPENDENCIES = host-autoconf
 
 # Makefile expects $STRIP -o to work, so needed for !BR2_STRIP_strip
 KEXEC_MAKE_OPTS = STRIP="$(TARGET_CROSS)strip"
@@ -32,5 +35,28 @@ define KEXEC_REMOVE_LIB_TOOLS
 endef
 
 KEXEC_POST_INSTALL_TARGET_HOOKS += KEXEC_REMOVE_LIB_TOOLS
+
+define KEXEC_CONFIGURE_CMDS
+	(cd $(@D); rm -rf config.cache; \
+		$(TARGET_MAKE_ENV) ./bootstrap; \
+		$(TARGET_CONFIGURE_ARGS) \
+		$(TARGET_CONFIGURE_OPTS) \
+		$(TARGET_MAKE_ENV) ./configure \
+		--host=$(GNU_TARGET_NAME) \
+		--prefix=/usr \
+	)
+endef
+
+define KEXEC_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D)
+endef
+
+define KEXEC_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D) DESTDIR=$(STAGING_DIR) install
+endef
+
+define KEXEC_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
 
 $(eval $(autotools-package))
