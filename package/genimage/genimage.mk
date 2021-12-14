c6660dcc9d63b541b69a671d079ff8e46f7d3e93
Dongjin Kim <tobetter@gmail.com>
2021-12-15 06:56:54 +0900

    ODROID-COMMON: package/genimage: update newer version 15
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>
    Change-Id: I0e19e53b4898bcfbb5e664e61ec47a5b014a9686

--- package/genimage/genimage.mk
+++ package/genimage/genimage.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-GENIMAGE_VERSION = 10
+GENIMAGE_VERSION = 15
 GENIMAGE_SOURCE = genimage-$(GENIMAGE_VERSION).tar.xz
-GENIMAGE_SITE = https://github.com/pengutronix/genimage/releases/download/v$(GENIMAGE_VERSION)
+GENIMAGE_SITE = https://public.pengutronix.de/software/genimage
 HOST_GENIMAGE_DEPENDENCIES = host-pkgconf host-libconfuse
 GENIMAGE_LICENSE = GPL-2.0
 GENIMAGE_LICENSE_FILES = COPYING
