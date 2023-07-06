316fcf2be42963634931cc5054aeb78bf83280ce
Dongjin Kim <tobetter@gmail.com>
2023-07-06 16:23:58 +0900

    ODROID-N2/C4: update boot script to use device node
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>
    Change-Id: Ia497dfcb6d789a807c98f9d6a3a13065c7d17f29

--- board/hardkernel/odroidn2/boot.cmd
+++ board/hardkernel/odroidn2/boot.cmd
@@ -4,14 +4,14 @@
 
 setenv voutmode "hdmi"
 
-setenv bootargs "root=UUID=@@ROOTUUID@@ rootfstype=ext4 rootwait init=/sbin/init console=ttyS0,115200 no_console_suspend earlyprintk=aml-uart,0xff803000 ramoops.pstore_en=1 ramoops.record_size=0x8000 ramoops.console_size=0x4000 voutmode=${voutmode} hdmimode=${hdmimode} cvbsmode=${cvbsmode} vdaccfg=${vdac_config}"
+setenv bootargs "root=/dev/mmcblk0p2 rootfstype=ext4 rootwait init=/sbin/init console=ttyS0,115200 no_console_suspend earlyprintk=aml-uart,0xff803000 ramoops.pstore_en=1 ramoops.record_size=0x8000 ramoops.console_size=0x4000 voutmode=${voutmode} hdmimode=${hdmimode} cvbsmode=${cvbsmode} vdaccfg=${vdac_config}"
 
 setenv fdt_addr_r "0x1000000"
 setenv kernel_addr_r "0x1080000"
 setenv kernel_addr_load "0x3000000"
 setenv ramdisk_addr_r "0x3080000"
 
-setenv fdtfile "meson64_odroidn2.dtb"
+setenv fdtfile "meson64_odroid${variant}.dtb"
 
 fatload mmc ${devno}:1 ${kernel_addr_load} Image.gz
 && unzip ${kernel_addr_load} ${kernel_addr_r}
