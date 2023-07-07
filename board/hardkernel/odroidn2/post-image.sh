285379b2c7886caa623a909f3a456c1c2b738afd
Dongjin Kim <tobetter@gmail.com>
2023-07-07 15:22:53 +0900

    Revert "ODROID-N2/C4: fix to use 'root=UUID=...' to kernel parameter"
    This reverts commit e1246726ac5385dac27cbf958276f4c0d1d6e236.

--- board/hardkernel/odroidn2/post-image.sh
+++ board/hardkernel/odroidn2/post-image.sh
@@ -2,19 +2,14 @@
 
 BOARD_DIR="$(dirname $0)"
 COMMON_DIR=${BOARD_DIR}/../common
-ROOTUUID=$(uuidgen)
 
-OSIMAGE=${BINARIES_DIR}/sdcard.img
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
-. ${COMMON_DIR}/functions
+rm -rf "${GENIMAGE_TMP}"
 
 cp ${COMMON_DIR}/logo.bmp.gz ${BINARIES_DIR}/
 cp ${BOARD_DIR}/boot.cmd ${BINARIES_DIR}/
-cp ${BOARD_DIR}/genimage.cfg ${BINARIES_DIR}/
-
-sed -i "s/@@ROOTUUID@@/${ROOTUUID}/g" \
-	${BINARIES_DIR}/boot.cmd \
-	${BINARIES_DIR}/genimage.cfg
 
 echo "ODROIDN2-UBOOT-CONFIG" > ${BINARIES_DIR}/boot.ini
 cat ${BINARIES_DIR}/boot.cmd >> ${BINARIES_DIR}/boot.ini
@@ -22,17 +17,11 @@ cat ${BINARIES_DIR}/boot.cmd >> ${BINARIES_DIR}/boot.ini
 ${HOST_DIR}/usr/bin/mkimage -A arm64 -T script -C none \
 	-d ${BINARIES_DIR}/boot.cmd ${BINARIES_DIR}/boot.scr
 
-yes | tune2fs -U ${ROOTUUID} ${BINARIES_DIR}/rootfs.ext4 || exit 1
-
-GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp" \
-	&& rm -rf "${GENIMAGE_TMP}"
 genimage                           \
 	--rootpath "${TARGET_DIR}"     \
 	--tmppath "${GENIMAGE_TMP}"    \
 	--inputpath "${BINARIES_DIR}"  \
 	--outputpath "${BINARIES_DIR}" \
-	--config "${BINARIES_DIR}/genimage.cfg"
-
-reset_uuid 2 ${ROOTUUID} ${BINARIES_DIR}/sdcard.img
+	--config "${GENIMAGE_CFG}"
 
 dd if=${BINARIES_DIR}/u-boot.bin of=${BINARIES_DIR}/sdcard.img bs=512 seek=1 conv=fsync,notrunc
